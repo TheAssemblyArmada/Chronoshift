0d5a91785fe9b6f5070cf1459cdbca9be4ba60f7
OmniBlade <nadia_xy@hotmail.com>
2017-08-26 23:10:39 +0100

    Implementes Read_Line functions.

---
+++ src/game/common/readline.cpp
@@ -0,0 +1,62 @@
+/**
+ * @file
+ *
+ * @Author CCHyper, OmniBlade
+ *
+ * @brief Reads a line of text from a file or straw.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "readline.h"
+#include "filestraw.h"
+#include "gamedebug.h"
+#include "stringex.h"
+
+int Read_Line(FileClass &file, char *line, int length, BOOL &eof)
+{
+    FileStraw fstraw(&file);
+    return Read_Line(fstraw, line, length, eof);
+}
+
+int Read_Line(Straw &straw, char *buffer, int length, BOOL &eof)
+{
+    DEBUG_ASSERT(buffer != nullptr);
+    DEBUG_ASSERT(length > 0);
+
+    if (buffer && length > 0) {
+        int i = 0;
+        char c;
+
+        while (true) {
+            if (straw.Get(&c, sizeof(c)) != sizeof(c)) {
+                eof = true;
+                break;
+            }
+
+            if (c == '\n') {
+                break;
+            }
+
+            if (c != '\r' && i + 1 < length) {
+                buffer[i++] = c;
+            }
+        }
+
+        // insert null char at the end of the line.
+        buffer[i] = '\0';
+
+        // trim trailing white space.
+        strtrim(buffer);
+
+        // return the length of the line read.
+        return strlen(buffer);
+    }
+
+    return 0;
+}
---
+++ src/game/common/readline.h
@@ -0,0 +1,28 @@
+/**
+ * @file
+ *
+ * @Author CCHyper, OmniBlade
+ *
+ * @brief Reads a line of text from a file or straw.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef READLINE_H
+#define READLINE_H
+
+#include "always.h"
+#include "fileclass.h"
+#include "straw.h"
+
+int Read_Line(FileClass &file, char *buffer, int length, BOOL &eof);
+int Read_Line(Straw &straw, char *buffer, int length, BOOL &eof);
+
+#endif // READLINE_H
