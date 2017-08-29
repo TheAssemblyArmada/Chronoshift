f18eac46f3567560991955fe9a09122d702c4065
OmniBlade <nadia_xy@hotmail.com>
2017-08-29 11:53:36 +0100

    Implements fixed and IndexClass.

---
+++ src/game/common/fixed.cpp
@@ -0,0 +1,144 @@
+/**
+ * @file
+ *
+ * @Author CCHyper, OmniBlade
+ *
+ * @brief Fixed point math class.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "fixed.h"
+#include "minmax.h"
+#include "stringex.h"
+#include <ctype.h>
+#include <stdio.h>
+
+fixed const fixed::_0_1(0, 1);
+fixed const fixed::_1_1(1, 1);
+fixed const fixed::_1_2(1, 2);
+fixed const fixed::_1_3(1, 3);
+fixed const fixed::_1_4(1, 4);
+fixed const fixed::_3_4(3, 4);
+fixed const fixed::_2_3(2, 3);
+
+fixed::fixed(int a, int b)
+{
+    if (b) {
+        m_number.word = (a << 8) / b;
+    } else {
+        m_number.word = 0;
+    }
+}
+
+fixed::fixed(char const *string)
+{
+    char const *str_read = string;
+
+    if (string) {
+        // this while loop finds first number while for loop finds
+        // one after last?
+        while (isspace(*str_read) != 0) {
+            ++str_read;
+        }
+
+        // Find something that isn't a number?
+        char const *i;
+
+        for (i = str_read; isdigit(*i) != 0; ++i) {
+            ;
+        }
+
+        if (*i == '%') {
+            m_number.word = (atoi(str_read) << 8) / 100;
+        } else {
+            m_number.word = 0;
+            if (string && *string != '.') {
+                m_number.ch.hi = atoi(string);
+            }
+
+            // finds '.' if it exists, nulls char *if not to indicate no
+            // decimal
+            while (*str_read != '.') {
+                if (*str_read) {
+                    ++str_read;
+                    char cur_char = *str_read;
+                    if (*str_read == '.') {
+                        break;
+                    }
+                    ++str_read;
+
+                    if (cur_char) {
+                        continue;
+                    }
+                }
+                str_read = nullptr;
+                break;
+            }
+
+            str_read = str_read ? str_read + 1 : str_read;
+
+            if (str_read) {
+                int divisor = 1;
+                int fraction = atoi(str_read);
+
+                while (isdigit(*str_read) != 0) {
+                    ++str_read;
+                    divisor *= 10;
+                }
+
+                m_number.ch.low = (fraction << 8) / divisor;
+            }
+        }
+    } else {
+        m_number.word = 0;
+    }
+}
+
+int const fixed::To_ASCII(char *string, int size) const
+{
+    char char_buff[32];
+
+    if (!string) {
+        return 0;
+    }
+
+    int whole = m_number.ch.hi;
+    int fraction = (100 * m_number.ch.low) >> 8;
+
+    if (fraction > 0) {
+        snprintf(char_buff, sizeof(char_buff), "%d.%02d", whole, fraction);
+
+        char *last_char = &char_buff[(int)strlen(char_buff) - 1];
+
+        while (*last_char == '0') {
+            *last_char = '\0';
+            --last_char;
+        }
+    } else {
+        // sprintf( char_buff, "%d", whole );
+        sprintf(char_buff, "%d.0", whole);
+    }
+
+    if (size == -1) {
+        size = (int)strlen(char_buff) + 1;
+    }
+
+    strlcpy((char *)string, char_buff, size);
+
+    return Min((int)strlen(char_buff), (size - 1));
+}
+
+char *const fixed::As_ASCII(void) const
+{
+    static char _buffer[32];
+
+    To_ASCII(_buffer, sizeof(_buffer));
+
+    return _buffer;
+}
---
+++ src/game/common/fixed.h
@@ -0,0 +1,183 @@
+/**
+ * @file
+ *
+ * @Author CCHyper, OmniBlade
+ *
+ * @brief Fixed point math class.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef FIXED_H
+#define FIXED_H
+
+#include "always.h"
+#include "endiantype.h"
+
+class fixed
+{
+public:
+    friend int operator*(int const &a, fixed const &b);
+    friend int operator*(fixed const &a, int const &b);
+    friend bool operator>(fixed const &a, int const &b);
+    friend bool operator>(int const &a, fixed const &b);
+
+    struct Data
+    {
+        struct Composite
+        {
+#if defined(SYSTEM_BIG_ENDIAN)
+            uint8_t hi;
+            uint8_t low;
+#elif defined(SYSTEM_LITTLE_ENDIAN)
+            uint8_t low;
+            uint8_t hi;
+#endif //
+        };
+
+        // members
+        union
+        {
+            uint16_t word;
+            Composite ch;
+        };
+    };
+
+    fixed() {}
+    explicit fixed(uint8_t byte) { m_number.word = byte; }
+    fixed(int a, int b = 1);
+    fixed(char const *string);
+    fixed(fixed const &that) : m_number(that.m_number) {}
+    ~fixed() {}
+
+    operator int() const;
+    fixed &operator=(int const &num);
+    fixed &operator=(fixed const &that);
+    fixed operator+(fixed const &that) const;
+    fixed operator-(fixed const &that) const;
+    fixed operator/(fixed const &that) const;
+    fixed operator*(fixed const &that) const;
+    fixed &operator+=(fixed const &that) { return *this = *this + that; }
+    fixed &operator-=(fixed const &that) { return *this = *this - that; }
+    fixed &operator*=(fixed const &that) { return *this = *this * that; }
+    fixed &operator/=(fixed const &that) { return *this = *this / that; }
+    bool operator==(fixed const &that) const { return m_number.word == that.m_number.word; }
+    bool operator!=(fixed const &that) const { return m_number.word != that.m_number.word; }
+    bool operator<(fixed const &that) const { return m_number.word < that.m_number.word; }
+    bool operator>(fixed const &that) const { return that < *this; }
+    bool operator<=(fixed const &that) const { return !(*this > that); }
+    bool operator>=(fixed const &that) const { return !(*this < that); }
+
+    int const To_ASCII(char *string, int size) const;
+    char *const As_ASCII() const;
+    int Round_To_Int() const { return (m_number.word + 128) >> 8; }
+    int Chop_To_Int() const { return m_number.word >> 8; }
+    int Get_Word() const { return m_number.word; }
+
+    static fixed const _0_1; // 0.0	0
+    static fixed const _1_1; // 1.0	256
+    static fixed const _1_2; // 0.5
+    static fixed const _1_3; // 0.33 ish
+    static fixed const _1_4; // 0.25
+    static fixed const _3_4; // 0.75
+    static fixed const _2_3; // 0.66 ish
+
+private:
+    // This can be treated as a word or two chars
+    Data m_number;
+};
+
+inline fixed::operator int() const
+{
+    Data tmp;
+    tmp.word = m_number.word + 128;
+    return tmp.ch.hi;
+}
+
+inline fixed &fixed::operator=(int const &num)
+{
+    m_number.ch.low = 0;
+    m_number.ch.hi = num;
+
+    return *this;
+}
+
+inline fixed &fixed::operator=(fixed const &that)
+{
+    m_number.word = that.m_number.word;
+
+    return *this;
+}
+
+inline fixed fixed::operator+(fixed const &that) const
+{
+    fixed tmp;
+    tmp.m_number.word = m_number.word + that.m_number.word;
+
+    return tmp;
+}
+
+inline fixed fixed::operator-(fixed const &that) const
+{
+    fixed tmp;
+    tmp.m_number.word = m_number.word - that.m_number.word;
+
+    return tmp;
+}
+
+inline fixed fixed::operator/(fixed const &that) const
+{
+    fixed tmp;
+    tmp.m_number.word = ((int)m_number.word << 8) / that.m_number.word;
+
+    return tmp;
+}
+
+inline fixed fixed::operator*(fixed const &that) const
+{
+    // Multiplication can overflow, largest whole value multi supported
+    // by 8:8 fixed point is around <16.0 * <16.0
+    fixed tmp;
+    tmp.m_number.word = ((int)m_number.word * that.m_number.word) >> 8;
+
+    return tmp;
+}
+
+inline int operator*(int const &a, fixed const &b)
+{
+    return (a * b.m_number.word + 128) / 256;
+}
+
+inline int operator*(fixed const &a, int const &b)
+{
+    return (b * a.m_number.word + 128) / 256;
+}
+
+inline bool operator>(fixed const &a, int const &b)
+{
+    return a.m_number.word > (b << 8);
+}
+
+inline bool operator>(int const &a, fixed const &b)
+{
+    return (a << 8) > b.m_number.word;
+}
+
+inline bool operator<(fixed const &a, int const &b)
+{
+    return b > a;
+}
+
+inline bool operator<(int const &a, fixed const &b)
+{
+    return b > a;
+}
+
+#endif // FIXED_H
