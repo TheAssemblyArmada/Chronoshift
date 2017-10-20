94e66173742f9b348b4a0ea46622736e677574e6
OmniBlade <nadia_xy@hotmail.com>
2017-10-20 21:23:55 +0100

    Implements CheckboxClass.

---
+++ src/game/ui/checkbox.cpp
@@ -0,0 +1,57 @@
+/**
+ * @file
+ *
+ * @author CCHyper
+ * @author OmniBlade
+ *
+ * @brief Class representing a ehckbox element.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "checkbox.h"
+#include "dialog.h"
+#include "mouse.h"
+
+BOOL CheckBoxClass::Draw_Me(BOOL redraw)
+{
+    if (ControlClass::Draw_Me(redraw)) {
+        g_mouse->Hide_Mouse();
+        Draw_Box(XPos, YPos, Width, Height, BOX_STYLE_0, false);
+        // This appears to be how it was originally
+        // LogicPage->Fill_Rect(XPos + 1, YPos + 1,
+        //					 XPos + Width - 2, YPos + Height - 2,
+        //					 13);
+        // if ( ToggleState ) {
+        //	LogicPage->Fill_Rect(XPos + 1, YPos + 1,
+        //						 XPos + Width - 2, YPos + Height - 2,
+        //						 4);
+        //}
+
+        // This way only calls Fill_Rect once.
+        int fill_color = ToggleState ? 4 : 13;
+        g_logicPage->Fill_Rect(XPos + 1, YPos + 1, XPos + Width - 2, YPos + Height - 2, fill_color);
+        g_mouse->Show_Mouse();
+
+        return true;
+    }
+
+    return false;
+}
+
+BOOL CheckBoxClass::Action(unsigned flags, KeyNumType &key)
+{
+    if (flags & MOUSE_LEFT_RLSE) {
+        if (ToggleState) {
+            Turn_Off();
+        } else {
+            Turn_On();
+        }
+    }
+
+    return ToggleClass::Action(flags, key);
+}
---
+++ src/game/ui/checkbox.h
@@ -0,0 +1,44 @@
+/**
+ * @file
+ *
+ * @author CCHyper
+ * @author OmniBlade
+ *
+ * @brief Class representing a ehckbox element.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#ifndef CHECKBOX_H
+#define CHECKBOX_H
+
+#include "always.h"
+#include "toggle.h"
+
+class CheckBoxClass : public ToggleClass
+{
+public:
+    CheckBoxClass(unsigned id, int x, int y, int w, int h) : ToggleClass(id, x, y, w, h) {}
+    CheckBoxClass(CheckBoxClass &that) : ToggleClass(that) {}
+    virtual ~CheckBoxClass() {}
+
+    virtual BOOL Draw_Me(BOOL redraw) override;
+    virtual BOOL Action(unsigned flags, KeyNumType &key) override;
+
+    CheckBoxClass &operator=(CheckBoxClass &that);
+};
+
+inline CheckBoxClass &CheckBoxClass::operator=(CheckBoxClass &that)
+{
+    if (this != &that) {
+        ToggleClass::operator=(that);
+    }
+
+    return *this;
+}
+
+#endif // CHECKBOX_H
