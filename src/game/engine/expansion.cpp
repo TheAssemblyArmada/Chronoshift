984e5360174f57014dc03d438a4248ee6807ca30
OmniBlade <nadia_xy@hotmail.com>
2017-11-22 22:04:22 +0000

    Adds doc block for expansion.cpp.

--- src/game/engine/expansion.cpp
+++ src/game/engine/expansion.cpp
@@ -1,3 +1,18 @@
+/**
+ * @file
+ *
+ * @author CCHyper
+ * @author OmniBlade
+ *
+ * @brief Functions for handling things relating to expansion packs.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
 #include "expansion.h"
 
 BOOL Is_Counterstrike_Installed()
