ea771a9a6eceaedf8b86047c73b3c7ec026ae386
OmniBlade <nadia_xy@hotmail.com>
2017-11-22 22:46:14 +0000

    Implements GetCDClass.

---
+++ src/game/engine/getcd.cpp
@@ -0,0 +1,60 @@
+/**
+ * @file
+ *
+ * @author tomsons26
+ *
+ * @brief Class for finding the CD drive.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "getcd.h"
+
+#ifndef RAPP_STANDALONE
+GetCDClass &g_cdList = Make_Global<GetCDClass>(0x00680884);
+#else
+GetCDClass g_cdList;
+#endif
+
+GetCDClass::GetCDClass()
+{
+#ifdef PLATFORM_WINDOWS
+    char drive_name[20];
+
+    strcpy(drive_name, "a:\\");
+    Set_Drive_Count(0);
+    Set_Current_Drive(0);
+    memset(m_drivelist, INVALID_CD_DRIVE, sizeof(m_drivelist));
+
+    for (char driveletter = 'a'; driveletter <= 'z'; ++driveletter) {
+        drive_name[0] = driveletter;
+
+        if(GetDriveType(drive_name) == DRIVE_CDROM) {
+            m_drivelist[m_driveCount++] = driveletter - 'a';
+        }
+    }
+#endif
+}
+
+int GetCDClass::Get_CD_Drive()
+{
+    if (Get_Drive_Count()) {
+        if (m_currentDrive == Get_Drive_Count()) {
+            Set_Current_Drive(0);
+        }
+
+        return m_drivelist[m_currentDrive++];
+    }
+
+    return INVALID_CD_DRIVE;
+}
+
+int GetCDClass::Reset_And_Get_CD_Drive()
+{
+    Set_Current_Drive(0);
+    return Get_CD_Drive();
+}
---
+++ src/game/engine/getcd.h
@@ -0,0 +1,52 @@
+/**
+ * @file
+ *
+ * @author tomsons26
+ *
+ * @brief Class for finding the CD drive.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+
+#pragma once
+
+#ifndef GETCD_H
+#define GETCD_H
+
+#include "always.h"
+
+class GetCDClass
+{
+    enum
+    {
+        INVALID_CD_DRIVE = -1,
+    };
+public:
+    GetCDClass();
+    ~GetCDClass(){};
+
+    int Get_Drive_Count() { return m_driveCount; }
+    void Set_Drive_Count(int count) { m_driveCount = count; }
+    int Get_CD_Drive();
+    int Reset_And_Get_CD_Drive();
+    void Set_Current_Drive(int drive) { m_currentDrive = drive; }
+
+private:
+    int m_drivelist[26];
+    int m_driveCount;
+    int m_currentDrive;
+};
+
+#ifndef RAPP_STANDALONE
+#include "hooker.h"
+extern GetCDClass &g_cdList;
+#else
+extern GetCDClass g_cdList;
+#endif
+
+#endif // GETCD_H
