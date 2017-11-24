15d2cca74bf49036a135b048d92d52b286cdfdf7
OmniBlade <nadia_xy@hotmail.com>
2017-11-24 23:27:29 +0000

    Adds comment block for gscreen.cpp.

--- src/game/engine/gscreen.cpp
+++ src/game/engine/gscreen.cpp
@@ -1,3 +1,18 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ * @author CCHyper
+ *
+ * @brief Base of the IOMap hierachy responsible for handling message list drawing.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
 #include "gscreen.h"
 #include "gamedebug.h"
 #include "gbuffer.h"
