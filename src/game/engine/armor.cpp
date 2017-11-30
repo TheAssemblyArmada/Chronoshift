1ee1372f39f1127133fd8ddc60a61c7d8ec046c4
OmniBlade <nadia_xy@hotmail.com>
2017-11-30 23:10:23 +0000

    Implements Armor enum and functions.

---
+++ src/game/engine/armor.cpp
@@ -0,0 +1,41 @@
+/**
+ * @file
+ *
+ * @author CCHyper
+ * @author OmniBlade
+ *
+ * @brief Armor enum and functions.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "armor.h"
+#include "gamedebug.h"
+
+const char *ArmorName[ARMOR_COUNT] = { "none", "wood", "light", "heavy", "concrete" };
+
+ArmorType Armor_From_Name(const char *name)
+{
+    if (name != nullptr) {
+        for (ArmorType armor = ARMOR_NONE; armor < ARMOR_COUNT; ++armor) {
+            if (strcasecmp(name, ArmorName[armor]) == 0) {
+                return armor;
+            }
+        }
+    }
+
+    return ARMOR_NONE;
+}
+
+const char *Name_From_Armor(ArmorType armor)
+{
+    if (armor >= ARMOR_NONE && armor < ARMOR_COUNT) {
+        return ArmorName[armor];
+    }
+
+    return "none";
+}
---
+++ src/game/engine/armor.h
@@ -0,0 +1,38 @@
+/**
+ * @file
+ *
+ * @author CCHyper
+ * @author OmniBlade
+ *
+ * @brief Armor enum and functions.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef ARMOR_H
+#define ARMOR_H
+
+#include	"always.h"
+
+enum ArmorType {
+    ARMOR_INVALID = -1,
+    ARMOR_NONE,
+    ARMOR_WOOD,
+    ARMOR_LIGHT,
+    ARMOR_HEAVY,
+    ARMOR_CONCRETE,
+    ARMOR_COUNT,
+};
+
+DEFINE_ENUMERATION_OPERATORS(ArmorType);
+
+ArmorType Armor_From_Name(const char *name);
+const char *Name_From_Armor(ArmorType armor);
+
+#endif // ARMOR_H
