7874eebc2f61b545636eaacb9c75f584596c75b1
OmniBlade <nadia_xy@hotmail.com>
2018-04-04 12:00:05 +0100

    Implements TechnoTypeClass.

---
+++ src/game/engine/technotype.cpp
@@ -0,0 +1,424 @@
+/**
+ * @file
+ *
+ * @author CCHyper
+ * @author OmniBlade
+ *
+ * @brief Techno layer of type classes, basis for all player unit type classes.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "technotype.h"
+#include "ccfileclass.h"
+#include "cell.h"
+#include "iomap.h"
+#include "lists.h"
+#include "mixfile.h"
+#include "rules.h"
+#include "session.h"
+
+#ifndef RAPP_STANDALONE
+void *&TechnoTypeClass::WakeShapes = Make_Global<void *>(0x0068D2DC);
+void *&TechnoTypeClass::TurretShapes = Make_Global<void *>(0x0068D2E0);
+void *&TechnoTypeClass::SamShapes = Make_Global<void *>(0x0068D2E4);
+void *&TechnoTypeClass::MGunShapes = Make_Global<void *>(0x0068D2E8);
+#else
+void *TechnoTypeClass::WakeShapes = nullptr;
+void *TechnoTypeClass::TurretShapes = nullptr;
+void *TechnoTypeClass::SamShapes = nullptr;
+void *TechnoTypeClass::MGunShapes = nullptr;
+#endif
+void *TechnoTypeClass::LightningShapes = nullptr;
+void *TechnoTypeClass::MissingCameoShape = nullptr;
+
+/**
+ * 0x00569564
+ */
+TechnoTypeClass::TechnoTypeClass(RTTIType type, int id, int uiname, char const *name, RemapType altremap, int def_fire_coord,
+    int pri_fire_coord_a, int pri_fire_coord_b, int sec_fire_coord_a, int sec_fire_coord_b, BOOL nominal,
+    BOOL radar_invisible, BOOL selectable, BOOL legal_target, BOOL insignificant, BOOL is_immune, BOOL theater, BOOL turret,
+    BOOL remapable, BOOL logical, int facings, SpeedType speed) :
+    ObjectTypeClass(
+        type, id, nominal, radar_invisible, selectable, legal_target, insignificant, is_immune, logical, uiname, name),
+    Remap(altremap),
+    DoubleOwned(false),
+    IsInvisible(false),
+    IsLeader(false),
+    IsScanner(false),
+    IsNominal(nominal),
+    IsTheater(theater),
+    IsTurretEquipped(turret),
+    IsRepairable(true),
+    IsCrewed(false),
+    IsRemapable(remapable),
+    Cloakable(false),
+    IsSelfHealing(false),
+    Explodes(false),
+    MovementZone(MZONE_NORMAL),
+    Passengers(0),
+    Sight(0),
+    Cost(0),
+    TechLevel(0),
+    Prerequisite(0),
+    Risk(0),
+    Value(0),
+    MPH((MPHType)0),
+    Speed(speed),
+    Ammo(-1),
+    Owner(OWNER_NONE),
+    CameoData(nullptr),
+    FacingsCount(facings),
+    ROT(0),
+    Primary(nullptr),
+    Secondary(nullptr),
+    DefualtFireCoord(def_fire_coord),
+    PrimaryFireCoordA(pri_fire_coord_a),
+    PrimaryFireCoordB(pri_fire_coord_b),
+    SecondaryFireCoordA(sec_fire_coord_a),
+    SecondaryFireCoordB(sec_fire_coord_b),
+    Points(0)
+{
+}
+
+TechnoTypeClass::TechnoTypeClass(const TechnoTypeClass &that) :
+    ObjectTypeClass(that),
+    Remap(that.Remap),
+    DoubleOwned(that.DoubleOwned),
+    IsInvisible(that.IsInvisible),
+    IsLeader(that.IsLeader),
+    IsScanner(that.IsScanner),
+    IsNominal(that.IsNominal),
+    IsTheater(that.IsTheater),
+    IsTurretEquipped(that.IsTurretEquipped),
+    IsRepairable(that.IsRepairable),
+    IsCrewed(that.IsCrewed),
+    IsRemapable(that.IsRemapable),
+    Cloakable(that.Cloakable),
+    IsSelfHealing(that.IsSelfHealing),
+    Explodes(that.Explodes),
+    MovementZone(that.MovementZone),
+    GuardRange(that.GuardRange),
+    Passengers(that.Passengers),
+    Sight(that.Sight),
+    Cost(that.Cost),
+    TechLevel(that.TechLevel),
+    Prerequisite(that.Prerequisite),
+    Risk(that.Risk),
+    Value(that.Value),
+    MPH(that.MPH),
+    Speed(that.Speed),
+    Owner(that.Owner),
+    CameoData(that.CameoData),
+    FacingsCount(that.FacingsCount),
+    ROT(that.ROT),
+    Primary(that.Primary),
+    Secondary(that.Secondary),
+    DefualtFireCoord(that.DefualtFireCoord),
+    PrimaryFireCoordA(that.PrimaryFireCoordA),
+    PrimaryFireCoordB(that.PrimaryFireCoordB),
+    SecondaryFireCoordA(that.SecondaryFireCoordA),
+    SecondaryFireCoordB(that.SecondaryFireCoordB),
+    Points(that.Points)
+{
+}
+
+TechnoTypeClass &TechnoTypeClass::operator=(TechnoTypeClass &that)
+{
+    if (this != &that) {
+        ObjectTypeClass::operator=(that);
+        Remap = that.Remap;
+        DoubleOwned = that.DoubleOwned;
+        IsInvisible = that.IsInvisible;
+        IsLeader = that.IsLeader;
+        IsScanner = that.IsScanner;
+        IsNominal = that.IsNominal;
+        IsTheater = that.IsTheater;
+        IsTurretEquipped = that.IsTurretEquipped;
+        IsRepairable = that.IsRepairable;
+        IsCrewed = that.IsCrewed;
+        IsRemapable = that.IsRemapable;
+        Cloakable = that.Cloakable;
+        IsSelfHealing = that.IsSelfHealing;
+        Explodes = that.Explodes;
+        MovementZone = that.MovementZone;
+        GuardRange = that.GuardRange;
+        Passengers = that.Passengers;
+        Sight = that.Sight;
+        Cost = that.Cost;
+        TechLevel = that.TechLevel;
+        Prerequisite = that.Prerequisite;
+        Risk = that.Risk;
+        Value = that.Value;
+        MPH = that.MPH;
+        Speed = that.Speed;
+        Owner = that.Owner;
+        CameoData = that.CameoData;
+        FacingsCount = that.FacingsCount;
+        ROT = that.ROT;
+        Primary = that.Primary;
+        Secondary = that.Secondary;
+        DefualtFireCoord = that.DefualtFireCoord;
+        PrimaryFireCoordA = that.PrimaryFireCoordA;
+        PrimaryFireCoordB = that.PrimaryFireCoordB;
+        SecondaryFireCoordA = that.SecondaryFireCoordA;
+        SecondaryFireCoordB = that.SecondaryFireCoordB;
+        Points = that.Points;
+    }
+
+    return *this;
+}
+
+/**
+ * @brief Does this object fire two shots per attack?
+ *
+ * 0x005698E8
+ */
+BOOL TechnoTypeClass::Is_Two_Shooter() const
+{
+    // TODO Requires WeaponTypeClass
+#ifndef RAPP_STANDALONE
+    BOOL (*func)(const TechnoTypeClass *) = reinterpret_cast<BOOL (*)(const TechnoTypeClass *)>(0x005698E8);
+    return func(this);
+#elif 0
+    return Primary != nullptr && (Primary == Secondary || Primary->Burst > 1);
+#else
+    return false;
+#endif
+}
+
+/**
+ * @brief Would this object be legally places at given cellnum?
+ *
+ * 0x00569E30
+ */
+BOOL TechnoTypeClass::Legal_Placement(int16_t cellnum) const
+{
+    if (cellnum != -1) {
+        for (int16_t *list_pos = Occupy_List(true); list_pos[0] == LIST_END; ++list_pos) {
+            int16_t offset_cellnum = (list_pos[0] + cellnum);
+
+            if (!Map.In_Radar(offset_cellnum)) {
+                return false;
+            }
+
+            CellClass &cell = Map[offset_cellnum];
+
+            if (What_Am_I() == RTTI_BUILDINGTYPE) {
+                if (!cell.Is_Clear_To_Build(Speed)) {
+                    return false;
+                }
+
+            } else {
+                if (!cell.Is_Clear_To_Move(Speed, false, false, -1, MZONE_NORMAL)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+/**
+ * @brief Returns bitfield of allowed owners for this object.
+ *
+ * 0x00569754
+ */
+int TechnoTypeClass::Get_Ownable() const
+{
+    if (DoubleOwned && Session.Game_To_Play() != GAME_CAMPAIGN) {
+        return Owner | (SIDE_ALLIES | SIDE_SOVIET);
+    }
+
+    return Owner;
+}
+
+/**
+ * @brief Returns build time (in ticks or seconds?).
+ *
+ * 0x00569784
+ */
+int TechnoTypeClass::Time_To_Build() const
+{
+    return fixed(900, 1000) * Cost * Rule.Get_Build_Speed();
+}
+
+/**
+ * @brief Gets the sidebar icon shape data for this object.
+ *
+ * 0x005697E8
+ */
+void *TechnoTypeClass::Get_Cameo_Data() const
+{
+    if (CameoData != nullptr) {
+        return CameoData;
+    }
+
+    // Enhancement backported from TS.
+    if (MissingCameoShape != nullptr) {
+        return MissingCameoShape;
+    }
+
+    return nullptr;
+}
+
+/**
+ * @brief Gets cost of repairing this object in credits.
+ *
+ * 0x005697F8
+ */
+int TechnoTypeClass::Repair_Cost() const
+{
+    if (What_Am_I() == RTTI_INFANTRYTYPE || What_Am_I() == RTTI_UNITTYPE || What_Am_I() == RTTI_VESSELTYPE
+        || What_Am_I() == RTTI_AIRCRAFTTYPE) {
+        return (Raw_Cost() / Strength / Rule.Get_Unit_Repair_Step())
+            * Rule.Get_Unit_Repair_Percent(); // this code check if the result is greater than 1(.0), if so,
+                                              // forces 1, see for TS/RA2 version
+                                              // http://dc.strategy-x.com/src2/TechnoTypeClass/GetRepairStepCost.cpp needs
+                                              // recoding for use with fixed *see (U)RepairPercent type.
+    }
+
+    return (Raw_Cost() / Strength / Rule.Get_Repair_Step())
+        * Rule.Get_Repair_Percent(); // same as above, but this is normal repair percent for buildings.
+}
+
+/**
+ * @brief Gets how much is repaired per repair "tick".
+ *
+ * 0x005698AC
+ */
+int TechnoTypeClass::Repair_Step() const
+{
+    if (What_Am_I() == RTTI_INFANTRYTYPE || What_Am_I() == RTTI_UNITTYPE || What_Am_I() == RTTI_VESSELTYPE
+        || What_Am_I() == RTTI_AIRCRAFTTYPE) {
+        return Rule.Get_Unit_Repair_Step();
+    }
+
+    return Rule.Get_Repair_Step();
+}
+
+/**
+ * @brief Reads the information from the objects ini entry that pertains to the TechnoType layer.
+ *
+ * 0x00569914
+ */
+BOOL TechnoTypeClass::Read_INI(CCINIClass &ini)
+{
+    // TODO Requires WeaponTypeClass and more functions in CCINIClass
+#ifndef RAPP_STANDALONE
+    BOOL (*func)
+    (const TechnoTypeClass *, CCINIClass &) = reinterpret_cast<BOOL (*)(const TechnoTypeClass *, CCINIClass &)>(0x005698E8);
+    return func(this, ini);
+#elif 0
+    if (ObjectTypeClass::Read_INI(ini)) {
+        DoubleOwned = ini.Get_Bool(Get_Name(), "DoubleOwned", DoubleOwned);
+        GuardRange = ini.Get_Lepton(Get_Name(), "GuardRange", GuardRange);
+        Explodes = ini.Get_Bool(Get_Name(), "Explodes", Explodes);
+        Primary =
+            WeaponTypeClass::As_Pointer(ini.Get_WeaponType(Get_Name(), "Primary", (Primary ? Primary->Type : WEAPON_NONE)));
+        Secondary = WeaponTypeClass::As_Pointer(
+            ini.Get_WeaponType(Get_Name(), "Secondary", (Secondary ? Secondary->Type : WEAPON_NONE)));
+        Cloakable = ini.Get_Bool(Get_Name(), "Cloakable", Cloakable);
+        IsScanner = ini.Get_Bool(Get_Name(), "Sensors", IsScanner);
+        Prerequisite = ini.Get_Buildings(Get_Name(), "Prerequisite", Prerequisite);
+        Sight = ini.Get_Int(Get_Name(), "Sight", Sight);
+        TechLevel = ini.Get_Int(Get_Name(), "TechLevel", TechLevel);
+
+        // MPH
+        // TODO, two versions of code, maybe we need to fix Get_MPH?, see CCINI
+        fixed def(MPH, 256);
+        int value = ini.Get_MPHType(Get_Name(), "Speed", MPH);
+
+        if (value != -1) {
+            value = Bound(value, 0, 100);
+
+            int retval = (value * 255) / 100;
+
+            if (retval >= 255) {
+                retval = -1;
+            }
+
+            MPH = (MPHType)retval;
+        }
+
+        Cost = ini.Get_Int(Get_Name(), "Cost", Cost);
+        Ammo = ini.Get_Int(Get_Name(), "Ammo", Ammo);
+        Points = ini.Get_Int(Get_Name(), "Points", Points);
+        Owner = ini.Get_Owners(Get_Name(), "Owner", Owner);
+        IsCrewed = ini.Get_Bool(Get_Name(), "Crewed", IsCrewed);
+        IsRepairable = ini.Get_Bool(Get_Name(), "Repairable", IsRepairable);
+        IsInvisible = ini.Get_Bool(Get_Name(), "Invisible", IsInvisible);
+        IsSelfHealing = ini.Get_Bool(Get_Name(), "SelfHealing", IsSelfHealing);
+        ROT = ini.Get_Int(Get_Name(), "ROT", ROT);
+        Passengers = ini.Get_Int(Get_Name(), "Passengers", Passengers);
+        Value = Points;
+
+        if (Primary != nullptr && Primary->Damage > 0) {
+            IsLeader = true;
+        } else {
+            IsLeader = false;
+        }
+        if (Primary && Primary->Warhead && Primary->Warhead->Wall) { // need confirming
+            MovementZone = MZONE_DESTROYER;
+        }
+
+        if (Speed == SPEED_FLOAT) {
+            MovementZone = MZONE_AMPHIBIOUS_DESTROYER;
+        }
+
+        return true;
+    }
+
+    return false;
+#else
+    return false;
+#endif
+}
+
+/**
+ * @brief Writes the information to the objects ini entry that pertains to the TechnoType layer.
+ */
+BOOL TechnoTypeClass::Write_INI(CCINIClass &ini) const
+{
+    // TODO
+    return false;
+}
+
+/**
+ * @brief Performs one time initialisation of TechnoType related data.
+ */
+void TechnoTypeClass::One_Time()
+{
+    // NOTE, Moved from UnitTypeClass as it was actually setting TechnoType statics. Call from UnitTypeClass One_Time().
+    if (!WakeShapes) {
+        WakeShapes = MixFileClass<CCFileClass>::Retrieve("WAKE.SHP");
+        DEBUG_ASSERT(WakeShapes != nullptr);
+    }
+    if (!TurretShapes) {
+        TurretShapes = MixFileClass<CCFileClass>::Retrieve("TURR.SHP");
+        DEBUG_ASSERT(TurretShapes != nullptr);
+    }
+    if (!SamShapes) {
+        SamShapes = MixFileClass<CCFileClass>::Retrieve("SSAM.SHP");
+        DEBUG_ASSERT(SamShapes != nullptr);
+    }
+    if (!MGunShapes) {
+        MGunShapes = MixFileClass<CCFileClass>::Retrieve("MGUN.SHP");
+        DEBUG_ASSERT(MGunShapes != nullptr);
+    }
+    if (!LightningShapes) {
+        LightningShapes = MixFileClass<CCFileClass>::Retrieve("LITNING.SHP");
+        DEBUG_ASSERT(LightningShapes != nullptr);
+    }
+    if (!MissingCameoShape) {
+        MissingCameoShape = MixFileClass<CCFileClass>::Retrieve("XXICON.SHP");
+        DEBUG_ASSERT(MissingCameoShape != nullptr);
+    }
+}
---
+++ src/game/engine/technotype.h
@@ -0,0 +1,142 @@
+/**
+ * @file
+ *
+ * @author CCHyper
+ * @author OmniBlade
+ *
+ * @brief Techno layer of type classes, basis for all player unit type classes.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef TECHNOTYPE_H
+#define TECHNOTYPE_H
+
+#include "always.h"
+#include "ccini.h"
+#include "gametypes.h"
+#include "land.h"
+#include "mzone.h"
+#include "objecttype.h"
+#include "remap.h"
+
+class TechnoTypeClass : public ObjectTypeClass
+{
+public:
+    TechnoTypeClass(RTTIType type, int id, int uiname, const char *name, RemapType remap, int def_fire_coord,
+        int pri_fire_coord_a, int pri_fire_coord_b, int sec_fire_coord_a, int sec_fire_coord_b, BOOL crushable,
+        BOOL radar_invisible, BOOL selectable, BOOL legal_target, BOOL insignificant, BOOL is_immune, BOOL theater,
+        BOOL turret, BOOL remapable, BOOL logical, int facings, SpeedType speed);
+
+    TechnoTypeClass(TechnoTypeClass const &that);
+    TechnoTypeClass(NoInitClass const &noinit) : ObjectTypeClass(noinit) {}
+    ~TechnoTypeClass() {} // TODO not virtual in original binary, consider making it so after standalone?
+
+    TechnoTypeClass &operator=(TechnoTypeClass &that);
+
+    virtual int Get_Ownable() const;
+    virtual int Cost_Of() const { return Cost; }
+    virtual int Time_To_Build() const;
+    virtual void *Get_Cameo_Data() const;
+    virtual int Raw_Cost() const { return Cost; }
+    virtual int Max_Passengers() const { return Passengers; }
+    virtual int Repair_Cost() const;
+    virtual int Repair_Step() const;
+    virtual BOOL Read_INI(CCINIClass &ini);
+
+    BOOL Write_INI(CCINIClass &ini) const;
+    BOOL Is_Two_Shooter() const;
+    BOOL Legal_Placement(int16_t cellnum) const;
+
+    static void One_Time();
+
+protected:
+    RemapType Remap;
+#ifndef RAPP_NO_BITFIELDS
+    // Union/Struct required to get correct packing when compiler packing set to 1.
+    union
+    {
+        struct
+        {
+            bool DoubleOwned : 1; // 1 Can be built/owned by all countries in a multiplayer game (def = false)?
+            bool IsInvisible : 1; // 2 confirmed or RadarVisible, depending on how its used.
+            bool IsLeader : 1; // 4 99.9% sure, its set when a object has a valid Primary with Damage greater than 0.
+            bool IsScanner : 1; // 8 In TS this is Sensors?
+            bool IsNominal : 1; // 16 Always use the given name rather than generic "enemy object" (def = false)?
+            bool IsTheater : 1; // 32 Does it have theater specific imagery (def = false)?
+            bool IsTurretEquipped : 1; // 64
+            bool IsRepairable : 1; // 128
+            bool IsCrewed : 1; // 1 Does it contain a crew that can escape [never infantry] (def = false)?
+            bool IsRemapable : 1; // 2
+            bool Cloakable : 1; // 4 or NoMovingFire?
+            bool IsSelfHealing : 1; // 8 Does the object heal automatically up to half strength (def = false)?
+            bool Explodes : 1; // 16 Does it explode violently when destroyed(def = false)?
+        };
+        int Bitfield;
+    };
+#else
+    bool DoubleOwned; // Can be built/owned by all countries in a multiplayer game (def = false)?
+    bool IsInvisible; // confirmed or RadarVisible, depending on how its used.
+    bool IsLeader; // 99.9% sure, its set when a object has a valid Primary with Damage greater than 0. see 00569CCA
+    bool IsScanner; // In TS this is Sensors?
+    bool IsNominal; // Always use the given name rather than generic "enemy object" (def = false)?
+    bool IsTheater; // Does it have theater specific imagery (def = false)?
+    bool IsTurretEquipped;
+    bool IsRepairable;
+    bool IsCrewed; // Does it contain a crew that can escape [never infantry] (def = false)?
+    bool IsRemapable;
+    bool Cloakable; // or NoMovingFire?
+    bool IsSelfHealing; // Does the object heal automatically up to half strength (def = false)?
+    bool Explodes; // Does it explode violently when destroyed [i.e., does it do collateral damage] (def = false)?
+#endif
+    MZoneType MovementZone;
+    int GuardRange; // Distance to scan for enemies to attack (def = use weapon range)
+    int Passengers; // Number of passengers it may carry (def = 0)
+    int Sight; // Sight range, in cells (def=1)
+    int Cost; // Cost to build object (in credits) (def = 0)
+    int TechLevel; // Tech level required to build this [-1 means can't build] (def = -1)
+    uint32_t Prerequisite; // List of buildings needed before this can be manufactured (def = no requirement)
+    int Risk; // ThreatPosed in TS?
+    int Value; // this is a copy of 'Points', see value being added to other values at 00567512, name guessed.
+    MPHType MPH; // Speed of the unit, value relates to rules.ini value but is a transformed value.
+    SpeedType Speed; // Movement speed type for purposes of adjustments.
+    int Ammo; // Number of rounds carried between reloads [-1 means unlimited] (def = -1)
+    uint32_t Owner; // bitfield of who can build objects of this type.
+    void *CameoData; // Shape data for the side bar icon.
+    int FacingsCount; // Number of facing stages (def = 32)?
+    int ROT; // Rate Of Turn for body (if present) and turret (if present) (def = 0)
+    void *Primary; // TODO Should be WeaponTypeClass *// Primary weapon equipped with (def = none)
+    void *Secondary; // TODO Should be WeaponTypeClass *// Secondary weapon equipped with (def = none)
+    uint32_t DefualtFireCoord;
+    uint32_t PrimaryFireCoordA;
+    uint32_t PrimaryFireCoordB;
+    uint32_t SecondaryFireCoordA;
+    uint32_t SecondaryFireCoordB;
+    int Points;
+
+#ifndef RAPP_STANDALONE
+    static void *&WakeShapes;
+    static void *&TurretShapes;
+    static void *&SamShapes;
+    static void *&MGunShapes;
+#else
+    static void *WakeShapes;
+    static void *TurretShapes;
+    static void *SamShapes;
+    static void *MGunShapes;
+#endif
+    static void *LightningShapes; // New with RA++
+    static void *MissingCameoShape; // New with RA++
+};
+
+#ifndef RAPP_STANDALONE
+#include "hooker.h"
+#endif
+
+#endif // TECHNOTYPE_H
