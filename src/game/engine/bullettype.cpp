f32077693a5de114a8f98121f3925daa581b655a
OmniBlade <nadia_xy@hotmail.com>
2018-04-05 09:23:15 +0100

    Implements BulletTypeClass.

---
+++ src/game/engine/bullettype.cpp
@@ -0,0 +1,230 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ * @author CCHyper
+ *
+ * @brief Class containing information about projectile behaviour.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "bullettype.h"
+#include "ccfileclass.h"
+#include "mixfile.h"
+#include <cstdio>
+
+using std::snprintf;
+
+#ifndef RAPP_STANDALONE
+TFixedIHeapClass<BulletTypeClass> &BulletTypes = Make_Global<TFixedIHeapClass<BulletTypeClass> >(0x0065DE54);
+#else
+TFixedIHeapClass<BulletTypeClass> BulletTypes;
+#endif
+
+// If you want to add a bullet type, add it hear, to the enum in bullettype.h and Init_Heap further down. Order must match.
+const BulletTypeClass BulletInvisible(BULLET_INVISIBLE, "Invisible");
+const BulletTypeClass BulletCannon(BULLET_CANNON, "Cannon");
+const BulletTypeClass BulletAck(BULLET_ACK, "Ack");
+const BulletTypeClass BulletTorpedo(BULLET_TORPEDO, "Torpedo");
+const BulletTypeClass BulletFROG(BULLET_FROG, "FROG");
+const BulletTypeClass BulletHeatSeeker(BULLET_HEATSEEKER, "HeatSeeker");
+const BulletTypeClass BulletLaserGuided(BULLET_LASERGUIDED, "LaserGuided");
+const BulletTypeClass BulletLobbed(BULLET_LOBBED, "Lobbed");
+const BulletTypeClass BulletBomblet(BULLET_BOMBLET, "Bomblet");
+const BulletTypeClass BulletBallistic(BULLET_BALLISTIC, "Ballistic");
+const BulletTypeClass BulletParachute(BULLET_PARACHUTE, "Parachute");
+const BulletTypeClass BulletFireball(BULLET_FIREBALL, "Fireball");
+const BulletTypeClass BulletLeapDog(BULLET_LEAPDOG, "LeapDog");
+const BulletTypeClass BulletCatapult(BULLET_CATAPULT, "Catapult");
+const BulletTypeClass BulletAAMissile(BULLET_AAMISSILE, "AAMissile");
+const BulletTypeClass BulletGPSSatellite(BULLET_GPSSATELLITE, "GPSSatellite");
+const BulletTypeClass BulletNukeUp(BULLET_NUKEUP, "NukeUp");
+const BulletTypeClass BulletNukeDown(BULLET_NUKEDOWN, "NukeDown");
+
+/**
+ * @brief
+ *
+ * 0x00426A20 Prototypes don't match but it fulfills same purpose, do not hook.
+ */
+BulletTypeClass::BulletTypeClass(BulletType bullet, const char *name) :
+    ObjectTypeClass(RTTI_BULLETTYPE, BulletTypes.ID(this), true, true, false, false, true, true, false, TXT_NULL, name),
+    High(false),
+    Shadow(true),
+    Arcing(false),
+    Dropping(false),
+    Inviso(false),
+    Proximity(false),
+    Animates(false),
+    Ranged(false),
+    Rotates(true),
+    Inaccurate(false),
+    Translucent(false),
+    AntiAir(false),
+    AntiGround(true),
+    AntiSubWarfare(false),
+    Degenerates(false),
+    UnderWater(false),
+    Parachuted(false),
+    Gigundo(false),
+    Type(bullet), // This was originally HeapID and the ctor only took name.
+    ROT(0),
+    Arm(0),
+    Frames(0)
+{
+    // Bit64 = true;	//TS shows what classes set this to true, needs debuging once logics are up and running.
+    // Bit128 = false;
+}
+
+/**
+ * @brief Initialises the object heap for this class.
+ *
+ * 0x00426ADC
+ */
+void BulletTypeClass::Init_Heap(void)
+{
+    // In the original binary, these where created with just names. this has been changed so all Bullets are created globaly
+    // and initialised like all other game objects.
+    // Order of construction must match enum in bullettypes.h
+    new BulletTypeClass(BulletInvisible);
+    new BulletTypeClass(BulletCannon);
+    new BulletTypeClass(BulletAck);
+    new BulletTypeClass(BulletTorpedo);
+    new BulletTypeClass(BulletFROG);
+    new BulletTypeClass(BulletHeatSeeker);
+    new BulletTypeClass(BulletLaserGuided);
+    new BulletTypeClass(BulletLobbed);
+    new BulletTypeClass(BulletBomblet);
+    new BulletTypeClass(BulletBallistic);
+    new BulletTypeClass(BulletParachute);
+    new BulletTypeClass(BulletFireball);
+    new BulletTypeClass(BulletLeapDog);
+    new BulletTypeClass(BulletCatapult);
+    new BulletTypeClass(BulletAAMissile);
+    new BulletTypeClass(BulletGPSSatellite);
+    new BulletTypeClass(BulletNukeUp);
+    new BulletTypeClass(BulletNukeDown);
+}
+
+/**
+ * @brief Carries out one time initialisation of persistant data.
+ *
+ * 0x00426C98
+ */
+void BulletTypeClass::One_Time(void)
+{
+    char filename[512];
+
+    for (BulletType bullet = BULLET_FIRST; bullet < BULLET_COUNT; ++bullet) {
+        BulletTypeClass *bptr = As_Pointer(bullet);
+
+        if (!bptr->Inviso) {
+            const char *name = bptr->ImageName[0] != '\0' ? bptr->ImageName : bptr->Get_Name();
+            snprintf(filename, sizeof(filename), "%s.shp", name);
+            bptr->ImageData = MixFileClass<CCFileClass>::Retrieve(filename);
+        }
+    }
+}
+
+/**
+ * 0x00426AB0
+ */
+void *BulletTypeClass::operator new(size_t size)
+{
+    return BulletTypes.Allocate();
+}
+
+/**
+ * 0x00426AC4
+ */
+void BulletTypeClass::operator delete(void *ptr)
+{
+    BulletTypes.Free(ptr);
+}
+
+/**
+ * @brief Get type enum value from a string.
+ */
+BulletType BulletTypeClass::From_Name(const char *name)
+{
+    DEBUG_ASSERT(name != nullptr);
+
+    if (strcasecmp(name, "<none>") == 0 || strcasecmp(name, "none") == 0) {
+        return BULLET_NONE;
+    }
+
+    if (name != nullptr) {
+        for (BulletType bullet = BULLET_FIRST; bullet < BULLET_COUNT; ++bullet) {
+            if (strcasecmp(name, Name_From(bullet)) == 0) {
+                return bullet;
+            }
+        }
+    }
+    return BULLET_NONE;
+}
+
+/**
+ * @brief Get type enum value from a string.
+ */
+const char *BulletTypeClass::Name_From(BulletType bullet)
+{
+    return bullet != BULLET_NONE && bullet < BULLET_COUNT ? As_Reference(bullet).Get_Name() : "<none>";
+}
+
+/**
+ * @brief Get reference to object from enum value.
+ *
+ * 0x00426D04
+ */
+BulletTypeClass &BulletTypeClass::As_Reference(BulletType bullet)
+{
+    return BulletTypes[bullet];
+}
+
+/**
+ * @brief Get pointer to object from enum value.
+ */
+BulletTypeClass *BulletTypeClass::As_Pointer(BulletType bullet)
+{
+    return bullet < BULLET_COUNT && bullet != BULLET_NONE ? &BulletTypes[bullet] : nullptr;
+}
+
+/**
+ * @brief Reads object data from an INI file.
+ *
+ * 0x00426D1C
+ */
+BOOL BulletTypeClass::Read_INI(CCINIClass &ini)
+{
+    if (ObjectTypeClass::Read_INI(ini)) {
+        Arm = ini.Get_Int(Get_Name(), "Arm", 0);
+        ROT = ini.Get_Int(Get_Name(), "ROT", 0);
+        Frames = ini.Get_Int(Get_Name(), "Frames", 1);
+        High = ini.Get_Bool(Get_Name(), "High", false);
+        Shadow = ini.Get_Bool(Get_Name(), "Shadow", true);
+        Arcing = ini.Get_Bool(Get_Name(), "Arcing", false);
+        Dropping = ini.Get_Bool(Get_Name(), "Dropping", false);
+        Inviso = ini.Get_Bool(Get_Name(), "Inviso", false);
+        Proximity = ini.Get_Bool(Get_Name(), "Proximity", false);
+        Animates = ini.Get_Bool(Get_Name(), "Animates", false);
+        Ranged = ini.Get_Bool(Get_Name(), "Ranged", false);
+        Inaccurate = ini.Get_Bool(Get_Name(), "Inaccuate", false);
+        AntiAir = ini.Get_Bool(Get_Name(), "AA", false);
+        AntiGround = ini.Get_Bool(Get_Name(), "AG", true);
+        AntiSubWarfare = ini.Get_Bool(Get_Name(), "ASW", false);
+        Degenerates = ini.Get_Bool(Get_Name(), "Degenerates", false);
+        UnderWater = ini.Get_Bool(Get_Name(), "UnderWater", false);
+        Parachuted = ini.Get_Bool(Get_Name(), "Parachuted", false);
+        Rotates = ini.Get_Bool(Get_Name(), "Rotates", false);
+        Translucent = ini.Get_Bool(Get_Name(), "Translucent", false);
+        Gigundo = ini.Get_Bool(Get_Name(), "Gigundo", false);
+
+        return true;
+    }
+
+    return false;
+}
---
+++ src/game/engine/bullettype.h
@@ -0,0 +1,144 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ * @author CCHyper
+ *
+ * @brief Class containing information about projectile behaviour.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef BULLETTYPE_H
+#define BULLETTYPE_H
+
+#include "always.h"
+#include "ccini.h"
+#include "heap.h"
+#include "objecttype.h"
+
+// If you add an entry here, add matching entries to bullettype.cpp globals and Init_Heap.
+enum BulletType
+{
+    BULLET_NONE = -1,
+    BULLET_FIRST = 0,
+    BULLET_INVISIBLE = 0,
+    BULLET_CANNON = 1,
+    BULLET_ACK = 2,
+    BULLET_TORPEDO = 3,
+    BULLET_FROG = 4,
+    BULLET_HEATSEEKER = 5,
+    BULLET_LASERGUIDED = 6,
+    BULLET_LOBBED = 7,
+    BULLET_BOMBLET = 8,
+    BULLET_BALLISTIC = 9,
+    BULLET_PARACHUTE = 10,
+    BULLET_FIREBALL = 11,
+    BULLET_LEAPDOG = 12,
+    BULLET_CATAPULT = 13,
+    BULLET_AAMISSILE = 14,
+    BULLET_GPSSATELLITE = 15,
+    BULLET_NUKEUP = 16,
+    BULLET_NUKEDOWN = 17,
+    BULLET_COUNT,
+};
+
+DEFINE_ENUMERATION_OPERATORS(BulletType);
+
+class BulletTypeClass : public ObjectTypeClass
+{
+public:
+    BulletTypeClass(BulletType bullet, const char *name);
+    BulletTypeClass(BulletTypeClass const &that) : ObjectTypeClass(that) {}
+    BulletTypeClass(NoInitClass const &noinit) : ObjectTypeClass(noinit) {}
+    ~BulletTypeClass() {}
+
+    void *operator new(size_t size);
+    void *operator new(size_t size, void *ptr) { return ptr; }
+    void operator delete(void *ptr);
+#ifndef COMPILER_WATCOM // Watcom doesn't like this, MSVC/GCC does.
+    void operator delete(void *ptr, void *place) {}
+#endif
+
+    virtual BOOL Create_And_Place(int16_t cellnum, HousesType house = HOUSES_NONE) const override { return false; };
+    virtual ObjectClass *Create_One_Of(HouseClass *house) const override { return nullptr; }
+    virtual BOOL Read_INI(CCINIClass &ini);
+
+    void Code_Pointers() {}
+    void Decode_Pointers() {}
+
+    static void Init_Heap();
+    static void One_Time();
+    static BulletType From_Name(const char *name);
+    static const char *Name_From(BulletType bullet);
+    static BulletTypeClass &As_Reference(BulletType bullet);
+    static BulletTypeClass *As_Pointer(BulletType bullet);
+
+private:
+#ifndef RAPP_NO_BITFIELDS
+    // Union/Struct required to get correct packing when compiler packing set to 1.
+    union
+    {
+        struct
+        {
+            bool High : 1; // Can it fly over walls?
+            bool Shadow : 1; // If High, does this bullet need to have a shadow drawn?
+            bool Arcing : 1; // Does it have a ballistic trajectory?
+            bool Dropping : 1; // Does it fall from a starting height?
+            bool Inviso : 1; // Is the projectile invisible as it travels?
+            bool Proximity : 1; // Does it blow up when near its target?
+            bool Animates : 1; // Does it animate [this means smoke puffs]?
+            bool Ranged : 1; // Can it run out of fuel?
+            bool Rotates : 1; // Does the projectile have rotation specific imagery?
+            bool Inaccurate : 1; // Is it inherently inaccurate?
+            bool Translucent : 1; // Are translucent colors used in artwork?
+            bool AntiAir : 1; // Can this weapon fire upon flying aircraft?
+            bool AntiGround : 1; // Can this weapon fire upon ground objects?
+            bool AntiSubWarfare : 1; // Is this an Anti-Submarine-Warfare projectile?
+            bool Degenerates : 1; // Does the bullet strength weaken as it travels?
+            bool UnderWater : 1; // Does the projectile travel under water?
+            bool Parachuted : 1; // Equipped with a parachute for dropping from plane (def = false)?
+            bool Gigundo : 1; // Is the projectile larger than normal?
+        };
+        int Bitfield;
+    };
+#else
+    bool High; // Can it fly over walls?
+    bool Shadow; // If High, does this bullet need to have a shadow drawn?
+    bool Arcing; // Does it have a ballistic trajectory?
+    bool Dropping; // Does it fall from a starting height?
+    bool Inviso; // Is the projectile invisible as it travels?
+    bool Proximity; // Does it blow up when near its target?
+    bool Animates; // Does it animate [this means smoke puffs]?
+    bool Ranged; // Can it run out of fuel?
+    bool Rotates; // Does the projectile have rotation specific imagery?
+    bool Inaccurate; // Is it inherently inaccurate?
+    bool Translucent; // Are translucent colors used in artwork?
+    bool AntiAir; // Can this weapon fire upon flying aircraft?
+    bool AntiGround; // Can this weapon fire upon ground objects?
+    bool AntiSubWarfare; // Is this an Anti-Submarine-Warfare projectile?
+    bool Degenerates; // Does the bullet strength weaken as it travels?
+    bool UnderWater; // Does the projectile travel under water?
+    bool Parachuted; // Equipped with a parachute for dropping from plane (def = false)?
+    bool Gigundo; // Is the projectile larger than normal?
+#endif 
+    BulletType Type;
+    uint8_t ROT; // Rate Of Turn [non zero implies homing].
+    int Arm; // Arming delay.
+    int Frames; // Number of image frames for animation purposes.
+};
+
+#ifndef RAPP_STANDALONE
+#include "hooker.h"
+extern TFixedIHeapClass<BulletTypeClass> &BulletTypes;
+#else
+extern TFixedIHeapClass<BulletTypeClass> BulletTypes;
+#endif
+
+#endif // BULLETTYPE_H
