e5651e3ad37b4f710af31978816a5505766272ae
CCHyper <none@none.com>
2018-04-10 23:38:27 +0100

    Added initial LogicClass, requires attention when other classes are implimented.

---
+++ src/game/engine/logic.cpp
@@ -0,0 +1,328 @@
+/**
+* @file
+*
+* @author CCHyper
+*
+* @brief <todo>
+*
+* @copyright RedAlert++ is free software: you can redistribute it and/or
+*            modify it under the terms of the GNU General Public License
+*            as published by the Free Software Foundation, either version
+*            2 of the License, or (at your option) any later version.
+*            A full copy of the GNU General Public License can be found in
+*            LICENSE
+*/
+#include "logic.h"
+#include "gamedebug.h"
+#include "iomap.h"
+//#include "voc.h"
+//#include "anim.h"
+//#include "trigger.h"
+//#include "house.h"
+#include "scenario.h"
+#include "session.h"
+//#include "factory.h"
+//#include "tevent.h"
+#include "object.h"
+//#include "anim.h"
+#include "target.h"
+
+LogicClass &Logic = Make_Global<LogicClass>(0x00668230);
+
+DynamicVectorClass<TriggerClass *> &LogicTriggers = Make_Global<DynamicVectorClass<TriggerClass *> >(0x0067F270);
+
+LogicClass::LogicClass() {}
+
+LogicClass::~LogicClass(void)
+{
+    LogicTriggers.Clear();
+}
+
+void LogicClass::Init(void)
+{
+    DEBUG_LOG("LogicClass::Clear()\n");
+
+    LayerClass::Init();
+}
+
+void LogicClass::Remove(ObjectClass *const object)
+{
+	LayerClass::Remove(object);
+}
+
+bool LogicClass::Submit(ObjectClass *object, bool sort)
+{
+    return LayerClass::Submit(object, sort);
+}
+
+/**
+* @brief AI processing entry function.
+*/
+void LogicClass::AI(void)
+{
+#ifndef RAPP_STANDALONE
+	void(*func)(LogicClass *) = reinterpret_cast<void(*)(LogicClass *)>(0x004FDD70);
+	return func(this);
+#else
+
+	//TODO: This function requires a lot of classes.
+
+    ++FramesPerSecond;
+
+    Scen.Do_Fade_AI();
+
+    for ( LogicTriggerID = 0; LogicTriggerID < LogicTriggers.Count(); ++LogicTriggerID ) {
+
+        //DEBUG_LOG("LogicClass::AI() - LogicTriggerID is %d, trigger name is '%s'\n", LogicTriggerID, LogicTriggers[LogicTriggerID]->ObjectType->Get_Name());
+
+        TriggerClass *trigger = LogicTriggers[LogicTriggerID];
+        DEBUG_ASSERT(trigger != nullptr);
+        if ( trigger != nullptr ) {
+
+            //DEBUG_LOG("LogicClass::AI() - LogicTrigger %d:'%s' is valid, about to check global status\n", LogicTriggerID, LogicTriggers[LogicTriggerID]->ObjectType->Get_Name());
+
+            if ( (!Scen._GlobalsChanged
+               || !trigger->Spring(TEVENT_GLOBAL_SET))
+               && !trigger->Spring(TEVENT_GLOBAL_CLEAR)
+
+              && (!Scen._DestroyBridges
+               || !trigger->Spring(TEVENT_ATTACHED_BRIDGE_DESTROYED))
+               && !trigger->Spring(TEVENT_ELAPSED_TIME) ) {
+
+                if (  Scen.GlobalTimer.Has_Expired() ) {
+                    trigger->Spring(TEVENT_TIMER_EXPIRED);
+                }
+
+            }
+
+        }
+
+    }
+
+    if ( Scen.GlobalTimer.Has_Expired() ) {
+        Scen.GlobalTimer.Stop();
+        Map.Flag_To_Redraw();
+    }
+
+    Scen._DestroyBridges = false;
+    Scen._GlobalsChanged = false;
+
+    if ( Special.Bools.ShroudGrows && Rule.ShroudRate > 0 ) {
+
+        if ( Scen.SomeScenarioTimer.Has_Expired() ) {
+
+            Scen.SomeScenarioTimer = (Rule.ShroudRate * DEF_TICKS_PER_MINUTE);
+
+            Map.Encroach_Shadow();
+        }
+
+    }
+
+    //
+    // 
+    //
+    //DEBUG_LOG("LogicClass::AI() - Teams.Count = %d\n", Teams.Count());
+    for ( int team = 0; team < Teams.Count(); ++team ) {
+        TeamClass *tptr = Teams[team];
+        DEBUG_ASSERT(tptr != nullptr);
+        if ( tptr != nullptr ) {
+            if ( tptr->Is_Active() ) {
+                //DEBUG_LOG("LogicClass::AI() - About to call AI() on team '%s'\n", tptr->Name());
+                tptr->AI();
+            }
+        }
+
+    }
+
+    if ( TimeQuake ) {
+
+        VocManager.Play_Locally(VOC_KABOOM15);
+
+        Shake_The_Screen(8);
+    }
+
+    ChronalVortex.AI();
+
+    for ( int index = 0; index < Logic.Count(); ++index ) {
+
+        ObjectClass *objptr = Logic[index];
+        DEBUG_ASSERT(objptr != nullptr);
+        if ( objptr != nullptr ) {
+
+            //DEBUG_LOG("LogicClass::AI() - About to call AI() on %s:%s\n", RTTIName[objptr->What_Am_I()].Name, objptr->Name());
+
+            objptr->AI();
+
+        }
+
+        if ( objptr != nullptr ) {
+
+            if ( TimeQuake ) {
+
+                DEBUG_LOG("LogicClass::AI() - Processing %s:%s for Time Quake damage!\n", RTTIName[objptr->What_Am_I()].Name, objptr->Name());
+
+                if ( objptr->Is_Active() && !objptr->In_Limbo() && objptr->Get_Health() > 0 ) {
+
+                    if ( TimeQuakeCenter != 0 ) {
+
+                        if ( Target_Distance(As_Target(objptr), TimeQuakeCenter) / 256 < Rule.MTankDistance ) {
+
+                            int v36 = 0;
+
+                            switch ( objptr->What_Am_I() ) {
+
+                                case RTTI_BUILDING:
+                                    v36 = objptr->Class_Of().Get_Strength() * Rule.QuakeBuildingDamage;
+                                    break;
+
+                                case RTTI_INFANTRY:
+                                    v36 = Rule.QuakeInfantryDamage;
+                                    break;
+
+                                case RTTI_VESSEL:
+                                    v36 = objptr->Class_Of().Get_Strength() * Rule.QuakeVesselDamage;
+                                    break;
+
+                                case RTTI_UNIT:
+                                    v36 = objptr->Class_Of().Get_Strength() * Rule.QuakeUnitDamage;
+                                    break;
+
+                                case RTTI_AIRCRAFT:
+                                {
+                                    //this chunk needs checking, its to make sure aircraft that are
+                                    //on the ground 
+                                    AircraftClass *aptr = dcast<AircraftClass *>(objptr);
+                                    if ( aptr != nullptr ) {
+                                        if ( aptr->On_Ground() ) {
+                                            //todo, new aircraft key
+                                            //v36 = objptr->Class_Of().Strength * Rule.QuakeAircraftDamage;
+                                        }
+                                    }
+                                    break;
+                                }
+
+                                default:
+                                    v36 = objptr->Class_Of().Get_Strength() * Rule.QuakeDamage;
+                                    break;
+
+                            };
+
+                            if ( v36 > 0 ) {
+
+                                objptr->Clicked_As_Target(7);
+
+                                AnimClass *anim = new AnimClass(ANIM_MINEEXP1, objptr->Center_Coord());
+                                DEBUG_ASSERT(anim != nullptr);
+
+                            }
+
+                            objptr->Take_Damage(v36, 0, WARHEAD_AP, nullptr, 1);   //TODO, check def args
+
+                        }
+
+                    }
+
+                }
+                
+            }
+
+        }
+
+        if ( objptr != Logic[index] ) {
+            --index;
+        }
+
+    }
+
+    HouseClass::Recalc_Attributes();
+
+    Map.Logic_AI();
+
+    //DEBUG_LOG("LogicClass::AI() - Factories.Count = %d\n", Teams.Count());
+    for ( int factory = 0; factory < Factories.Count(); ++factory ) {
+        FactoryClass *fptr = Factories[factory];
+        DEBUG_ASSERT(fptr != nullptr);
+        if ( fptr != nullptr ) {
+            if ( fptr->Is_Active() ) {
+                DEBUG_LOG("LogicClass::AI() - About to call FactoryClass::AI() on factory '%d'\n", factory);
+                fptr->AI();
+            }
+        }
+
+    }
+
+    if ( Session.GameToPlay != GAME_CAMPAIGN ) {
+
+        for ( HousesType mphouse = HOUSES_MULTI_FIRST; mphouse < HOUSES_MULTI_LAST; ++mphouse ) {
+
+            HouseClass *hptr = HouseClass::As_Pointer(mphouse);
+            DEBUG_ASSERT(hptr != nullptr);
+
+            if ( hptr != nullptr ) {
+                if ( hptr->Is_Active() ) {
+                    hptr->AI();
+                }
+            }
+
+        }
+
+    } else {
+
+        for ( HousesType house = HOUSES_FIRST; house < HOUSES_COUNT; ++house ) {
+
+            HouseClass *hptr = HouseClass::As_Pointer(house);
+            DEBUG_ASSERT(hptr != nullptr);
+            if ( hptr != nullptr ) {
+                if ( hptr->Is_Active() ) {
+                    hptr->AI();
+                }
+            }
+
+        }
+
+    }
+
+    if ( Session.GameToPlay != GAME_CAMPAIGN ) {
+
+        if ( Scen.AutoSonarTimer <= 0 ) {
+
+            if ( AutoSonarPulse ) {
+                Map.Activate_Pulse();
+                VocManager.Play_Locally(VOC_SONPULSE);
+                AutoSonarPulse = false;
+            }
+
+            Scen.AutoSonarTimer = 600;
+
+        }
+
+    }
+#endif
+}
+
+void LogicClass::Detach(target_t target, int a2)
+{
+#ifndef RAPP_STANDALONE
+	void(*func)(LogicClass *, target_t, int) = reinterpret_cast<void(*)(LogicClass *, target_t, int)>(0x004FE2BC);
+	return func(this, target, a2);
+#else
+
+	//TODO: This function requires TriggerClass implimentation.
+
+    if ( (RTTIType)a2 == RTTI_TRIGGER ) {
+
+        for ( int index = 0; index < LogicTriggers.Count(); ++index ) {
+
+            TriggerClass *ptr = LogicTriggers[index];
+            DEBUG_ASSERT(ptr != nullptr);
+
+            if ( As_Trigger(target) == ptr ) {
+
+                LogicTriggers.Delete_Index(index);
+            }
+
+        }
+
+    }
+#endif
+}
---
+++ src/game/engine/logic.h
@@ -0,0 +1,53 @@
+/**
+* @file
+*
+* @author CCHyper
+*
+* @brief <todo>
+*
+* @copyright RedAlert++ is free software: you can redistribute it and/or
+*            modify it under the terms of the GNU General Public License
+*            as published by the Free Software Foundation, either version
+*            2 of the License, or (at your option) any later version.
+*            A full copy of the GNU General Public License can be found in
+*            LICENSE
+*/
+#pragma once
+
+#ifndef LOGIC_H
+#define LOGIC_H
+
+#include	"always.h"
+#include	"layer.h"
+
+class ObjectClass;
+class TriggerClass;
+
+class LogicClass : public LayerClass
+{
+    public:
+        LogicClass(void);
+        virtual ~LogicClass(void);
+
+        void AI(void);
+
+        void Init(void);
+
+        void Remove(ObjectClass *const object);
+
+        bool Submit(ObjectClass *object, bool sort = false);
+
+        void Detach(target_t target, int a2);
+
+};
+
+#ifndef RAPP_STANDALONE
+#include "hooker.h"
+extern LogicClass &Logic;
+extern DynamicVectorClass<TriggerClass*> &LogicTriggers;
+#else
+extern LogicClass Logic;
+extern DynamicVectorClass<TriggerClass *> LogicTriggers;
+#endif
+
+#endif // LOGIC_H
