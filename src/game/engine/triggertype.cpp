7ec915a3e3f444d91d607f4eeeb736894150a36e
CCHyper <none@none.com>
2018-04-10 21:33:09 +0100

    TriggerTypeClass sources added, class still needs full implementation.

---
+++ src/game/engine/triggertype.cpp
@@ -0,0 +1,77 @@
+#include	"triggertype.h"
+
+#ifndef RAPP_STANDALONE
+TFixedIHeapClass<TriggerTypeClass> &TriggerTypes = *reinterpret_cast<TFixedIHeapClass<TriggerTypeClass> *>(0x0065DCD8);
+#else
+TFixedIHeapClass<TriggerTypeClass> TriggerTypes;
+#endif
+
+void TriggerTypeClass::Code_Pointers()
+{
+#ifndef RAPP_STANDALONE
+	void(*func)(TriggerTypeClass *) =
+		reinterpret_cast<void(*)(TriggerTypeClass *)>(0x004F9448);
+	return func(this);
+#else
+	//TODO: Requires TActionClass and TEventClass
+	//EventOne.Code_Pointers();
+	//EventTwo.Code_Pointers();
+	//ActionOne.Code_Pointers();
+	//ActionTwo.Code_Pointers();
+#endif
+}
+
+void TriggerTypeClass::Decode_Pointers()
+{
+#ifndef RAPP_STANDALONE
+	void(*func)(TriggerTypeClass *) =
+		reinterpret_cast<void(*)(TriggerTypeClass *)>(0x004F9464);
+	return func(this);
+#else
+	//TODO: Requires TActionClass and TEventClass
+	//EventOne.Decode_Pointers();
+	//EventTwo.Decode_Pointers();
+	//ActionOne.Decode_Pointers();
+	//ActionTwo.Decode_Pointers();
+#endif
+}
+
+TriggerTypeClass *TriggerTypeClass::From_Name(char const *name)
+{
+    if (strcasecmp(name, "<none>") == 0 || strcasecmp(name, "none") == 0) {
+        return nullptr;
+    }
+
+    if (name != nullptr) {
+        for (int index = 0; index < TriggerTypes.Count(); ++index) {
+            if (strcasecmp(name, Name_From((TriggerType)index)) == 0) {
+                return &TriggerTypes[index];
+            }
+        }
+    }
+
+    return nullptr;
+}
+
+char const *TriggerTypeClass::Name_From(TriggerType trigger)
+{
+    return (trigger != TRIGGER_NONE) && (trigger < TriggerTypes.Count()) ? As_Reference(trigger).Get_Name() : "<none>";
+}
+
+char const *TriggerTypeClass::Name_From(TriggerTypeClass *trigger)
+{
+    if (trigger != nullptr) {
+        return TriggerTypes[TriggerTypes.ID(trigger)].Get_Name();
+    }
+    return nullptr;
+}
+
+TriggerTypeClass &TriggerTypeClass::As_Reference(TriggerType trigger)
+{
+	return TriggerTypes[trigger];
+}
+
+TriggerTypeClass *TriggerTypeClass::As_Pointer(TriggerType trigger)
+{
+	return trigger != TRIGGER_NONE && trigger < TRIGGER_COUNT ? &TriggerTypes[trigger] : nullptr;
+}
---
+++ src/game/engine/triggertype.h
@@ -0,0 +1,121 @@
+#pragma once
+
+#ifndef TRIGGERTYPE_H
+#define TRIGGERTYPE_H
+
+#include "always.h"
+#include "abstracttype.h"
+#include "housetype.h"
+
+enum TriggerType
+{
+	TRIGGER_NONE = -1,
+
+	TRIGGER_FIRST = 0,
+
+	//TODO
+
+	TRIGGER_LAST = 999,
+
+	TRIGGER_COUNT = 999
+};
+
+DEFINE_ENUMERATION_OPERATORS(TriggerType);
+DEFINE_ENUMERATION_BITWISE_OPERATORS(TriggerType);
+
+//needs rename?
+/*
+//0 = Volatile
+//1 = Semi-persistant
+//2 = Persistent
+
+The first number tells RA whether this is a repeating trigger or not.
+If it has the value 0, then the trigger will only ever be activated once
+(not strictly true as the trigger can be fired again if associated with
+a teamtype).  If it has a value greater than 0, then the trigger is a
+repeating trigger (ie it will be fired more than once).
+
+For repeating triggers, there are two types.  When the repeating
+trigger has a value of 1, the trigger will only occur once the trigger
+event has happened to all items (units and buildings) to which this
+trigger has been attached.  This is useful if you want, for example,
+some action to occur after a specific set of buildings have been
+destroy.
+
+The second type of repeating trigger is the free repeater.  When this
+item has the value of 2, it will continue to repeat itself whenever its
+trigger event is true.  This is of use if you want, for example, to have
+the trigger activate every 20 time units.
+*/
+enum TriggerStateType
+{
+	STATE_NONE = -1,
+
+	STATE_FIRST = 0,
+
+	STATE_VOLATILE = 0,
+	STATE_SEMI_PERSISTANT = 1,
+	STATE_PERSISTANT = 2,
+
+	STATE_COUNT = 3
+};
+
+class TriggerTypeClass : public AbstractTypeClass
+{
+public:
+	TriggerTypeClass(NoInitClass const &noinit) : AbstractTypeClass(noinit) {}
+
+	void Code_Pointers();
+	void Decode_Pointers();
+
+    static TriggerTypeClass *From_Name(char const *name);
+    static char const *Name_From(TriggerType trigger);
+    static char const *Name_From(TriggerTypeClass *trigger);
+
+	static TriggerTypeClass &As_Reference(TriggerType trigger);
+	static TriggerTypeClass *As_Pointer(TriggerType trigger);
+
+protected:
+
+	//bitfield 0x25
+    bool IsActive;			// 1
+    //bool TrigTypeBit2;	// 2
+    //bool TrigTypeBit4;	// 4
+    //bool TrigTypeBit8;	// 8
+    //bool TrigTypeBit16;	// 16
+    //bool TrigTypeBit32;	// 32
+    //bool TrigTypeBit64;	// 64
+    //bool TrigTypeBit128;	// 128
+
+    TriggerStateType RepetitionState;			//0x29
+    HousesType House;			//0x2A
+
+	//TODO: Requires TEventClass
+    //TEventClass EventOne;		//0x2B
+    //TEventClass EventTwo;		//0x34
+    char TrigEventWhen;		//0x3D		//could be a enum.
+            /*
+            When 0, only the first trigger event (part 5) must be true for the trigger to be activated.
+            When 1, the first (part 5) and second (part 8) must both be true for the trigger to be activated.
+            When 2, either the first trigger event or the second trigger event must be true, whichever comes first.
+            When 3, either the first trigger event or the second trigger event must be true. See the upcoming summary (next) for further details on how this operates.
+            */
+
+	//TODO: Requires TActionClass
+    //TActionClass ActionOne;	//0x3E
+    //TActionClass ActionTwo;	//0x4B
+    char TrigActionWhen;	//0x58		//could be a enum.
+            /*
+            When 0, only one trigger action is activated when the event is triggered. See the summary (next) for which trigger action is activated, and when.
+            When 1, both trigger actions are activated when the event is triggered.
+            */
+};
+
+#ifndef RAPP_STANDALONE
+#include "hooker.h"
+extern TFixedIHeapClass<TriggerTypeClass> &TriggerTypes;
+#else
+extern TFixedIHeapClass<TriggerTypeClass> TriggerTypes;
+#endif
+
+#endif // TRIGGERTYPE_H
