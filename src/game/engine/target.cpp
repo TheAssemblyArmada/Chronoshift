d08d8c3fc8ec6ad2687557974b62dcb21af49613
CCHyper <none@none.com>
2018-04-11 13:32:29 +0100

    bool to BOOL changes for Target, applied Clang formatting.

--- src/game/engine/target.cpp
+++ src/game/engine/target.cpp
@@ -6,7 +6,7 @@
  *
  * @brief Functions for converting to and from target values.
  *
- * @copyright Redalert++ is free software: you can redistribute it and/or
+ * @copyright RedAlert++ is free software: you can redistribute it and/or
  *            modify it under the terms of the GNU General Public License
  *            as published by the Free Software Foundation, either version
  *            2 of the License, or (at your option) any later version.
@@ -14,28 +14,28 @@
  *            LICENSE
  */
 #include "target.h"
+#include "cell.h"
 #include "coord.h"
 #include "object.h"
-#include "cell.h"
 //#include "techno.h"
 #include "technotype.h"
 //#include "triggertype.h"
 
-const bool Target_Is_Techno(target_t target)
+BOOL Target_Is_Techno(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	//TODO: Inlined in RA
-	return nullptr;
+    // TODO: Inlined in RA
+    return nullptr;
 #else
-	//TODO: Requires TechnoClass implementation.
-	TechnoClass *ptr = As_Techno(target);
-	return ptr != nullptr ? ptr->Is_Techno() : false;
+    // TODO: Requires TechnoClass implementation.
+    TechnoClass *ptr = As_Techno(target);
+    return ptr != nullptr ? ptr->Is_Techno() : false;
 #endif
 }
 
-const bool Target_Legal(target_t target)
+BOOL Target_Legal(target_t target)
 {
-	return (Target_Get_RTTI(target) != RTTI_NONE && Target_Get_RTTI(target) < RTTI_COUNT && Target_Get_ID(target) >= 0);
+    return (Target_Get_RTTI(target) != RTTI_NONE && Target_Get_RTTI(target) < RTTI_COUNT && Target_Get_ID(target) >= 0);
 }
 
 target_t As_Target(cell_t cellnum)
@@ -45,79 +45,78 @@ target_t As_Target(cell_t cellnum)
 
 target_t As_Target(CellClass *cell)
 {
-	return cell != nullptr ? As_Target(cell->Cell_Number()) : 0;
+    return cell != nullptr ? As_Target(cell->Cell_Number()) : 0;
 }
 
 target_t As_Target(coord_t coord)
 {
 #ifndef RAPP_STANDALONE
-	target_t(*func)(coord_t) = reinterpret_cast<target_t(*)(coord_t)>(0x00555930);
-	return func(coord);
+    target_t (*func)(coord_t) = reinterpret_cast<target_t (*)(coord_t)>(0x00555930);
+    return func(coord);
 #else
-	//TODO: OmniBlade please implement.
-	return 0;
+    // TODO: OmniBlade please implement.
+    return 0;
 #endif
 }
 
 TriggerClass *As_Trigger(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	TriggerClass *(*func)(target_t) = reinterpret_cast<TriggerClass *(*)(target_t)>(0x00554FFC);
-	return func(target);
+    TriggerClass *(*func)(target_t) = reinterpret_cast<TriggerClass *(*)(target_t)>(0x00554FFC);
+    return func(target);
 #else
-	//TODO: Requires TriggerClass implementation.
-	return Target_Get_RTTI(target) == RTTI_TRIGGER ? &Triggers[Target_Get_ID(target)] : nullptr;
+    // TODO: Requires TriggerClass implementation.
+    return Target_Get_RTTI(target) == RTTI_TRIGGER ? &Triggers[Target_Get_ID(target)] : nullptr;
 #endif
 }
 
 TriggerTypeClass *As_TriggerType(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	TriggerTypeClass *(*func)(target_t) = reinterpret_cast<TriggerTypeClass *(*)(target_t)>(0x005559CC);
-	return func(target);
+    TriggerTypeClass *(*func)(target_t) = reinterpret_cast<TriggerTypeClass *(*)(target_t)>(0x005559CC);
+    return func(target);
 #else
-	//TODO: Requires TriggerTypeClass implementation.
-	return Target_Get_RTTI(target) == RTTI_TRIGGERTYPE ? &TriggerTypes[Target_Get_ID(target)] : nullptr;
+    // TODO: Requires TriggerTypeClass implementation.
+    return Target_Get_RTTI(target) == RTTI_TRIGGERTYPE ? &TriggerTypes[Target_Get_ID(target)] : nullptr;
 #endif
 }
 
 TeamClass *As_Team(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	TeamClass *(*func)(target_t) = reinterpret_cast<TeamClass *(*)(target_t)>(0x00555034);
-	return func(target);
+    TeamClass *(*func)(target_t) = reinterpret_cast<TeamClass *(*)(target_t)>(0x00555034);
+    return func(target);
 #else
-	//TODO: Requires TeamClass implementation.
-	return Target_Get_RTTI(target) == RTTI_TEAM ? &Teams[Target_Get_ID(target)] : nullptr;
+    // TODO: Requires TeamClass implementation.
+    return Target_Get_RTTI(target) == RTTI_TEAM ? &Teams[Target_Get_ID(target)] : nullptr;
 #endif
 }
 
 TeamTypeClass *As_TeamType(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	TeamTypeClass *(*func)(target_t) = reinterpret_cast<TeamTypeClass *(*)(target_t)>(0x0055506C);
-	return func(target);
+    TeamTypeClass *(*func)(target_t) = reinterpret_cast<TeamTypeClass *(*)(target_t)>(0x0055506C);
+    return func(target);
 #else
-	//TODO: Requires TeamTypeClass implementation.
-	return Target_Get_RTTI(target) == RTTI_TEAMTYPE ? &TeamTypes[Target_Get_ID(target)] : nullptr;
+    // TODO: Requires TeamTypeClass implementation.
+    return Target_Get_RTTI(target) == RTTI_TEAMTYPE ? &TeamTypes[Target_Get_ID(target)] : nullptr;
 #endif
 }
 
 AbstractClass *As_Abstract(target_t target)
 {
-	return (AbstractClass *)As_Object(target);
+    return (AbstractClass *)As_Object(target);
 }
 
 ObjectClass *As_Object(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	ObjectClass *(*func)(target_t) = reinterpret_cast<ObjectClass *(*)(target_t)>(0x00555190);
-	return func(target);
+    ObjectClass *(*func)(target_t) = reinterpret_cast<ObjectClass *(*)(target_t)>(0x00555190);
+    return func(target);
 #else
     ObjectClass *objptr = nullptr;
-    
-    switch (Target_Get_RTTI(target)) {
 
+    switch (Target_Get_RTTI(target)) {
         case RTTI_AIRCRAFT:
             objptr = As_Aircraft(target);
             break;
@@ -152,11 +151,10 @@ ObjectClass *As_Object(target_t target)
 
         default:
             break;
+    };
 
-	};
+    // DEBUG_ASSERT(objptr != nullptr);
 
-    //DEBUG_ASSERT(objptr != nullptr);
-    
     if (objptr != nullptr && objptr->Is_Active()) {
         return objptr;
     }
@@ -165,141 +163,140 @@ ObjectClass *As_Object(target_t target)
 #endif
 }
 
-
 AnimClass *As_Animation(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	AnimClass *(*func)(target_t) = reinterpret_cast<AnimClass *(*)(target_t)>(0x005550A4);
-	return func(target);
+    AnimClass *(*func)(target_t) = reinterpret_cast<AnimClass *(*)(target_t)>(0x005550A4);
+    return func(target);
 #else
-	//TODO: Requires AnimClass implementation.
-	return Target_Get_RTTI(target) == RTTI_ANIM ? &Anims[Target_Get_ID(target)] : nullptr;
+    // TODO: Requires AnimClass implementation.
+    return Target_Get_RTTI(target) == RTTI_ANIM ? &Anims[Target_Get_ID(target)] : nullptr;
 #endif
 }
 
 BulletClass *As_Bullet(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	BulletClass *(*func)(target_t) = reinterpret_cast<BulletClass *(*)(target_t)>(0x005550DC);
-	return func(target);
+    BulletClass *(*func)(target_t) = reinterpret_cast<BulletClass *(*)(target_t)>(0x005550DC);
+    return func(target);
 #else
-	//TODO: Requires BulletClass implementation.
-	return Target_Get_RTTI(target) == RTTI_BULLET ? &Bullets[Target_Get_ID(target)] : nullptr;
+    // TODO: Requires BulletClass implementation.
+    return Target_Get_RTTI(target) == RTTI_BULLET ? &Bullets[Target_Get_ID(target)] : nullptr;
 #endif
 }
 
 TerrainClass *As_Terrain(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	//TODO: Inlined in RA
-	return nullptr;
+    // TODO: Inlined in RA
+    return nullptr;
 #else
-	//TODO: Requires TerrainClass implementation.
-	return Target_Get_RTTI(target) == RTTI_TERRAIN ? &Terrains[Target_Get_ID(target)] : nullptr;
+    // TODO: Requires TerrainClass implementation.
+    return Target_Get_RTTI(target) == RTTI_TERRAIN ? &Terrains[Target_Get_ID(target)] : nullptr;
 #endif
 }
 
 TechnoClass *As_Techno(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	TechnoClass *(*func)(target_t) = reinterpret_cast<TechnoClass *(*)(target_t)>(0x0055514C);
-	return func(target);
+    TechnoClass *(*func)(target_t) = reinterpret_cast<TechnoClass *(*)(target_t)>(0x0055514C);
+    return func(target);
 #else
-	ObjectClass *objptr = As_Object(target);
-	DEBUG_ASSERT(objptr != nullptr);
-	return objptr != nullptr && objptr->Is_Techno() ? (TechnoClass *)objptr : nullptr;
+    ObjectClass *objptr = As_Object(target);
+    DEBUG_ASSERT(objptr != nullptr);
+    return objptr != nullptr && objptr->Is_Techno() ? (TechnoClass *)objptr : nullptr;
 #endif
 }
 
 TechnoTypeClass *As_TechnoType(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	TechnoTypeClass *(*func)(target_t) = reinterpret_cast<TechnoTypeClass *(*)(target_t)>(0x00555964);
-	return func(target);
+    TechnoTypeClass *(*func)(target_t) = reinterpret_cast<TechnoTypeClass *(*)(target_t)>(0x00555964);
+    return func(target);
 #else
-	//TODO: Requires all derivant TechnoTypeClass's to be implemented.
-	return nullptr;
+    // TODO: Requires all derivant TechnoTypeClass's to be implemented.
+    return nullptr;
 #endif
 }
 
 AircraftClass *As_Aircraft(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	AircraftClass *(*func)(target_t) = reinterpret_cast<AircraftClass *(*)(target_t)>(0x00555114);
-	return func(target);
+    AircraftClass *(*func)(target_t) = reinterpret_cast<AircraftClass *(*)(target_t)>(0x00555114);
+    return func(target);
 #else
-	//TODO: Requires AircraftClass implementation.
-	return 0;
+    // TODO: Requires AircraftClass implementation.
+    return 0;
 #endif
 }
 
 UnitClass *As_Unit(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	UnitClass *(*func)(target_t) = reinterpret_cast<UnitClass *(*)(target_t)>(0x00555290);
-	return func(target);
+    UnitClass *(*func)(target_t) = reinterpret_cast<UnitClass *(*)(target_t)>(0x00555290);
+    return func(target);
 #else
-	//TODO: Requires UnitClass implementation.
-	return 0;
+    // TODO: Requires UnitClass implementation.
+    return 0;
 #endif
 }
 
 VesselClass *As_Vessel(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	VesselClass *(*func)(target_t) = reinterpret_cast<VesselClass *(*)(target_t)>(0x005552C8);
-	return func(target);
+    VesselClass *(*func)(target_t) = reinterpret_cast<VesselClass *(*)(target_t)>(0x005552C8);
+    return func(target);
 #else
-	//TODO: Requires VesselClass implementation.
-	return nullptr;
+    // TODO: Requires VesselClass implementation.
+    return nullptr;
 #endif
 }
 
 InfantryClass *As_Infantry(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	InfantryClass *(*func)(target_t) = reinterpret_cast<InfantryClass *(*)(target_t)>(0x00555300);
-	return func(target);
+    InfantryClass *(*func)(target_t) = reinterpret_cast<InfantryClass *(*)(target_t)>(0x00555300);
+    return func(target);
 #else
-	//TODO: Requires InfantryClass implementation.
-	return nullptr;
+    // TODO: Requires InfantryClass implementation.
+    return nullptr;
 #endif
 }
 
 BuildingClass *As_Building(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	BuildingClass *(*func)(target_t) = reinterpret_cast<BuildingClass *(*)(target_t)>(0x00555338);
-	return func(target);
+    BuildingClass *(*func)(target_t) = reinterpret_cast<BuildingClass *(*)(target_t)>(0x00555338);
+    return func(target);
 #else
-	//TODO: Requires BuildingClass implementation.
-	return nullptr;
+    // TODO: Requires BuildingClass implementation.
+    return nullptr;
 #endif
 }
 
 cell_t As_Cell(target_t target)
 {
-	return Coord_To_Cell(As_Coord(target));
+    return Coord_To_Cell(As_Coord(target));
 }
 
 coord_t As_Coord(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	coord_t(*func)(target_t) = reinterpret_cast<coord_t(*)(target_t)>(0x00555384);
-	return func(target);
+    coord_t (*func)(target_t) = reinterpret_cast<coord_t (*)(target_t)>(0x00555384);
+    return func(target);
 #else
-	//TODO: OmniBlade please implement.
-	return 0;
+    // TODO: OmniBlade please implement.
+    return 0;
 #endif
 }
 
 coord_t As_Movement_Coord(target_t target)
 {
 #ifndef RAPP_STANDALONE
-	coord_t(*func)(target_t) = reinterpret_cast<coord_t(*)(target_t)>(0x005553F8);
-	return func(target);
+    coord_t (*func)(target_t) = reinterpret_cast<coord_t (*)(target_t)>(0x005553F8);
+    return func(target);
 #else
-	//TODO: OmniBlade please implement.
-	return 0;
+    // TODO: OmniBlade please implement.
+    return 0;
 #endif
 }
