54a2c28310120ace1c2e1f1e2f320c11532ccc2e
CCHyper <none@none.com>
2018-04-11 11:57:26 +0100

    Initial MessageBox source.

---
+++ src/game/ui/msgbox.h
@@ -0,0 +1,51 @@
+/**
+ * @file
+ *
+ * @author CCHyper
+ *
+ * @brief <todo>
+ *
+ * @copyright RedAlert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef MSGBOX_H
+#define MSGBOX_H
+
+#include "always.h"
+#include "language.h"
+
+enum MessageBoxReturn
+{
+    MSGBOX_IDFAIL = 0,
+    MSGBOX_IDOK = 1,
+    MSGBOX_IDCANCEL = 2,
+    MSGBOX_IDABORT = 3,
+    MSGBOX_IDRETRY = 4,
+    MSGBOX_IDIGNORE = 5,
+    MSGBOX_IDYES = 6,
+    MSGBOX_IDNO = 7
+};
+
+class MessageBoxClass
+{
+public:
+    MessageBoxClass(int caption = TXT_NONE) : CaptionText(caption) {}
+    ~MessageBoxClass() {}
+
+    int Process(const int body_text, const int button_1_text = TXT_OK, const int button_2_text = TXT_NONE, const int button_3_text = TXT_NONE, BOOL a5 = false);
+    int Process(const char *body_text, const int button_1_text = TXT_OK, const int button_2_text = TXT_NONE, const int button_3_text = TXT_NONE, BOOL a5 = false);
+
+private:
+    int Process(const char *body_text, const char *button_1_text, const char *button_2_text, const char *button_3_text, BOOL a5 = false);
+
+protected:
+    int CaptionText;
+};
+
+#endif // MSGBOX_H
