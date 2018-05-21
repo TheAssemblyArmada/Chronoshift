1d65ac58919adc5cc3554b6a7902de29f0f6f37a
Toms <tomsons26lv@gmail.com>
2018-05-21 19:07:33 +0300

    CreditClass doesn't have a dtor

--- src/game/engine/tab.h
+++ src/game/engine/tab.h
@@ -51,7 +51,6 @@ private:
     {
     public:
         CreditClass(void);
-        ~CreditClass(void);
 
         void Graphic_Logic(bool a1 = false);
         void AI(bool a1 = false);
@@ -91,7 +90,7 @@ public:
     void Flash_Money(void);
     void Draw_Passable_Tab(BOOL state);
     void Draw_Map_Size_Tab();
-    void Draw_Credits_Tab(void);
+    static void Draw_Credits_Tab(void);
     static void Hilite_Tab(TabEnum tab = TAB_NONE);
     void Set_Active(TabEnum tab = TAB_NONE);
 
