791f642fbfe9e6c56366104fc8ceb4162bde7eeb
OmniBlade <nadia_xy@hotmail.com>
2018-06-04 13:39:14 +0100

    Adds wrapper for Queue_Options functions.

---
+++ src/game/engine/queue.h
@@ -0,0 +1,29 @@
+/**
+ * @file
+ *
+ * @author CCHyper
+ * @author OmniBlade
+ *
+ * @brief Message queue related functions.
+ *
+ * @copyright Redalert++ is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "always.h"
+
+BOOL Queue_Options();
+
+#ifndef RAPP_STANDALONE
+#include "hooker.h"
+#endif
+
+#endif // QUEUE_H
