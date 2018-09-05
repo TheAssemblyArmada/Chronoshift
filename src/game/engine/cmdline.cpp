e6cca43979843de4176c5a4a336449527891c3fc
OmniBlade <nadia_xy@hotmail.com>
2018-09-05 08:44:05 +0100

    Adds command line parsing stub function.
    TODO to be further developed.

---
+++ src/game/engine/cmdline.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Command line parser.
+ *
+ * @copyright Chronoshift is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "cmdline.h"
+#include "globals.h"
+#include "special.h"
+
+BOOL Parse_Command_Line(int argc, char **argv)
+{
+    Special.Init();
+    Whom = HOUSES_GOODGUY;
+    g_inMapEditor = false;
+    DebugUnshroud = false;
+
+	// TODO decide what command line options we need to parse, original stuff might not make sense anymore.
+
+    return true;
+}
