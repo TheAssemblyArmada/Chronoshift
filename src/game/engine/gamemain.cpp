cc23bcf39ceb4bb1decdaee9191318dbe66d1705
OmniBlade <nadia_xy@hotmail.com>
2018-09-05 09:06:49 +0100

    Adds wrapper for game engine startup function.

---
+++ src/game/engine/gamemain.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief The game engines main routine to be called from the user entry point.
+ *
+ * @copyright Chronoshift is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "gamemain.h"
+
+void Game_Main(int argc, char **argv)
+{
+#ifndef CHRONOSHIFT_STANDALONE
+    void (*func)(int, char **) = reinterpret_cast<void (*)(int, char **)>(0x004A5368);
+    func(argc, argv);
+#endif
+}
---
+++ src/game/engine/gamemain.h
@@ -0,0 +1,24 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief The game engines main routine to be called from the user entry point.
+ *
+ * @copyright Chronoshift is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef GAMEMAIN_H
+#define GAMEMAIN_H
+
+#include "always.h"
+
+void Game_Main(int argc, char **argv);
+
+#endif // GAMEMAIN_H
