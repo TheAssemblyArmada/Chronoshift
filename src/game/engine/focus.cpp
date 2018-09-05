05826cb3cdf5b833fdefce81f23ebf1ea8685749
OmniBlade <nadia_xy@hotmail.com>
2018-09-05 09:06:10 +0100

    Adds focus handlers, adds some missing cpp to build.

---
+++ src/game/engine/focus.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Focus change handling functions.
+ *
+ * @copyright Chronoshift is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "focus.h"
+#include "audio.h"
+#include "gbuffer.h"
+#include "iomap.h"
+#include "mouse.h"
+#include "theme.h"
+
+void Focus_Loss()
+{
+    Theme.Suspend();
+    Stop_Primary_Sound_Buffer();
+
+	if (g_mouse != nullptr) {
+        g_mouse->Clear_Cursor_Clip();
+	}
+}
+
+void Focus_Restore()
+{
+    Map.Flag_To_Redraw(true);
+    Start_Primary_Sound_Buffer(true);
+
+	if (g_mouse != nullptr) {
+        g_mouse->Set_Cursor_Clip();
+    }
+
+	g_visiblePage.Clear();
+    g_hiddenPage.Clear();
+}
---
+++ src/game/engine/focus.h
@@ -0,0 +1,25 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Focus change handling functions.
+ *
+ * @copyright Chronoshift is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef FOCUS_H
+#define FOCUS_H
+
+#include "always.h"
+
+void Focus_Loss();
+void Focus_Restore();
+
+#endif // FOCUS_H
