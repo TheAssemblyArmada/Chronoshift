53d4689fb9ad995600455cf4c50a9f96cf35249a
OmniBlade <nadia_xy@hotmail.com>
2018-09-23 22:36:54 +0100

    Implements skeleton and wrapped functions for CargoClass.

---
+++ src/game/engine/cargo.h
@@ -0,0 +1,46 @@
+/**
+ * @file
+ *
+ * @author CCHyper
+ * @author OmniBlade
+ *
+ * @brief Handles tracking units currently stored as cargo.
+ *
+ * @copyright Chronoshift is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef CARGO_H
+#define CARGO_H
+
+#include "always.h"
+
+class FootClass;
+class NoInitClass;
+
+class CargoClass
+{
+public:
+    CargoClass() : Count(0), Object(nullptr) {}
+    CargoClass(NoInitClass const &noinit) {}
+    ~CargoClass() { Object = nullptr; }
+
+    void AI() {}
+    void Attach(FootClass *object);
+    FootClass *Attached_Object() const;
+    FootClass *Detach_Object();
+    void Code_Pointers();
+    void Decode_Pointers();
+    int Cargo_Count() const { return Count; }
+
+private:
+    uint8_t Count;
+    FootClass *Object;
+};
+
+#endif // CARGO_H
