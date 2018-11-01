34fd029d93045ef1b10b43971e394f7d6f861fd0
CCHyper <12141592+CCHyper@users.noreply.github.com>
2018-11-01 23:18:25 +0000

    Refactoring to RulesClass, added Get* functions for each member.

--- src/game/engine/rules.h
+++ src/game/engine/rules.h
@@ -29,6 +29,20 @@ class GameINIClass;
 
 struct IQStruct
 {
+/*public:
+    int Get_MaxIQLevels() const { return MaxIQLevels; }
+    int Get_SuperWeapons() const { return SuperWeapons; }
+    int Get_Production() const { return Production; }
+    int Get_GuardArea() const { return GuardArea; }
+    int Get_RepairSell() const { return RepairSell; }
+    int Get_AutoCrush() const { return AutoCrush; }
+    int Get_Scatter() const { return Scatter; }
+    int Get_ContentScan() const { return ContentScan; }
+    int Get_Aircraft() const { return Aircraft; }
+    int Get_Harvester() const { return Harvester; }
+    int Get_SellBack() const { return SellBack; }*/
+
+//private:
     int MaxIQLevels; // the maximum number of discrete IQ levels
     int SuperWeapons; // super weapons are automatically fired by computer
     int Production; // building/unit production is automatically controlled by computer
@@ -64,21 +78,186 @@ public:
     BOOL Objects(GameINIClass &ini);
     BOOL Difficulty(GameINIClass &ini);
 
-    BOOL Ore_Grows() const { return OreGrows; }
-    BOOL Ore_Spreads() const { return OreSpreads; }
-    BOOL Ally_Reveal() const { return AllyReveal; }
-    BOOL Named_Civilians() const { return NamedCivilians; }
-    BOOL Separate_Aircraft() const { return SeparateAircraft; }
+    const fixed &Turbo_Boost() const { return TurboBoost; }
+    const fixed &Attack_Interval() const { return AttackInterval; }
+    const fixed &Attack_Delay() const { return AttackDelay; }
+    const fixed &Power_Emergency() const { return PowerEmergency; }
+    int Badger_Bomb_Count() const { return BadgerBombCount; }
+    const fixed &Airstrip_Ratio() const { return AirstripRatio; }
+    int Airstrip_Limit() const { return AirstripLimit; }
+    const fixed &Helipad_Ratio() const { return HelipadRatio; }
+    int Helipad_Limit() const { return HelipadLimit; }
+    const fixed &Tesla_Ratio() const { return TeslaRatio; }
+    int Tesla_Limit() const { return TeslaLimit; }
+    const fixed &AA_Ratio() const { return AARatio; }
+    int AA_Limit() const { return AALimit; }
+    const fixed &Defense_Ratio() const { return DefenseRatio; }
+    int Defense_Limit() const { return DefenseLimit; }
+    const fixed &War_Factory_Ratio() const { return WarRatio; }
+    int War_Factory_Limit() const { return WarLimit; }
+    const fixed &Barracks_Ratio() const { return BarracksRatio; }
+    int Barracks_Limit() const { return BarracksLimit; }
+    int Refinery_Limit() const { return RefineryLimit; }
+    const fixed &Refinery_Ratio() const { return RefineryRatio; }
+    int Base_Size_Add() const { return BaseSizeAdd; }
+    int Power_Surplus() const { return PowerSurplus; }
+    int Infantry_Reserve() const { return InfantryReserve; }
+    int Infantry_Base_Mult() const { return InfantryBaseMult; }
+    const fixed &Chrono_Duration() const { return ChronoDuration; }
+    const fixed &Water_Crate_Chance() const { return WaterCrateChance; }
+    int Solo_Crate_Money() const { return SoloCrateMoney; }
+    int GPS_Tech_Level() const { return GPSTechLevel; }
+    UnitType Unit_CrateType() const { return UnitCrateType; }
+    const fixed &Patrol_Scan() const { return PatrolScan; }
+    const fixed &Team_Delay() const { return TeamDelay; }
+    const fixed &Submerge_Delay() const { return SubmergeDelay; }
+    const fixed &GameSpeed_Bias() const { return GameSpeedBias; }
+    const fixed &Base_Bias() const { return BaseBias; }
+    int16_t Vortex_Range() const { return VortexRange; }
+    MPHType Vortex_Speed() const { return VortexSpeed; }
+    int Vortex_Damage() const { return VortexDamage; }
+    const fixed &Vortex_Chance() const { return VortexChance; }
+    const fixed &Exp_Spread() const { return ExpSpread; }
+    int16_t Fire_Supress() const { return FireSupress; }
+    int Para_Tech() const { return ParaTech; }
+    int Spy_Plane_Tech() const { return SpyPlaneTech; }
+    int Parabomb_Tech() const { return ParabombTech; }
+    const IQStruct &IQ_Controls() const { return IQControls; }
+    CrateType Silver_Crate() const { return SilverCrate; }
+    CrateType Wood_Crate() const { return WoodCrate; }
+    CrateType Water_Crate() const { return WaterCrate; }
+    int Crate_Minimum() const { return CrateMinimum; }
+    int Crate_Maximum() const { return CrateMaximum; }
+    int16_t LZ_Scan_Radius() const { return LZScanRadius; }
+    int MPlayer_Money() const { return MPlayerMoney; }
+    int MPlayer_Max_Money() const { return MPlayerMaxMoney; }
+    bool MPlayer_ShadowGrow() const { return MPlayerShadowGrow; }
+    bool MPlayer_Bases() const { return MPlayerBases; }
+    bool MPlayer_OreGrows() const { return MPlayerOreGrows; }
+    bool MPlayer_Crates() const { return MPlayerCrates; }
+    bool MPlayer_AIPlayers() const { return MPlayerAIPlayers; }
+    bool MPlayer_Capture_The_Flag() const { return MPlayerCaptureTheFlag; }
+    int16_t Drop_Zone_Radius() const { return DropZoneRadius; }
+    const fixed &Message_Delay() const { return MessageDelay; }
+    const fixed &Savour_Delay() const { return SavourDelay; }
+    int AV_Mine_Damage() const { return AVMineDamage; }
+    int AP_Mine_Damage() const { return APMineDamage; }
+    int Max_Players() const { return Players; }
+    const fixed &Base_Defense_Delay() const { return BaseDefenseDelay; }
+    int Suspend_Priority() const { return SuspendPriority; }
+    const fixed &Suspend_Delay() const { return SuspendDelay; }
+    const fixed &Survivor_Rate() const { return SurvivorRate; }
+    const fixed &Reload_Rate() const { return ReloadRate; }
+    const fixed &Autocreate_Time() const { return AutocreateTime; }
+    const fixed &Buildup_Time() const { return BuildupTime; }
+    int Ore_Truck_Rate() const { return OreTruckRate; }
+    int Atom_Damage() const { return AtomDamage; }
+    const DifficultyClass &Difficulty_Control(DiffType diff) const { return Difficulties[diff]; }
+    bool Computer_Paranoid() const { return Paranoid; }
+    bool Aircraft_Curley_Shuffle() const { return CurleyShuffle; }
+    bool Flash_Low_Power() const { return FlashLowPower; }
+    bool Computer_Easy_Bonus() const { return CompEasyBonus; }
+    bool Fine_Diff_Control() const { return FineDiffControl; }
+    bool Is_Ore_Explosive() const { return OreExplosive; }
+    bool Show_Enemy_Health() const { return EnemyHealth; }
+    bool Allow_MCV_Undeploy() const { return MCVUndeploy; }
+    bool Ally_Reveal() const { return AllyReveal; }
+    bool Separate_Aircraft() const { return SeparateAircraft; }
+    bool Allow_Tree_Targeting() const { return TreeTargeting; }
+    bool Mine_Aware() const { return MineAware; }
+    bool Ore_Grows() const { return OreGrows; }
+    bool Ore_Spreads() const { return OreSpreads; }
+    bool Named_Civilians() const { return NamedCivilians; }
+    bool Player_Auto_Crush() const { return PlayerAutoCrush; }
+    bool Player_Return_Fire() const { return PlayerReturnFire; }
+    bool Player_Scatter() const { return PlayerScatter; }
+    bool Chrono_Kill_Cargo() const { return ChronoKillCargo; }
+    bool Secret_Units_Enabled() const { return SecretUnitsEnabled; }
+    bool New_Units_Enabled() const { return NewUnitsEnabled; }
+    const fixed &Prone_Damage() const { return ProneDamage; }
+    const fixed &Quake_Damage() const { return QuakeDamage; }
+    const fixed &Quake_Chance() const { return QuakeChance; }
     const fixed &Ore_Growth_Rate() const { return GrowthRate; }
-    int Get_Gap_Radius() const { return GapRadius; }
-    int Get_Gold_Value() const { return GoldValue; }
-    int Get_Gem_Value() const { return GemValue; }
-    const fixed &Get_Build_Speed() const { return BuildSpeed; }
+    const fixed &Shroud_Rate() const { return ShroudRate; }
+    const fixed &Crate_Regen() const { return CrateRegen; }
+    const fixed &Get_Timer_Warning() const { return TimerWarning; }
+    const fixed &Get_Recharge_Sonar() const { return RechargeSonar; }
+    const fixed &Get_Recharge_Chrono() const { return RechargeChrono; }
+    const fixed &Get_Recharge_ParaBomb() const { return RechargeParaBomb; }
+    const fixed &Get_Recharge_Paratrooper() const { return RechargeParatrooper; }
+    const fixed &Get_Recharge_ParaSaboteur() const { return RechargeParaSaboteur; }
+    const fixed &Get_Recharge_SpyPlane() const { return RechargeSpyPlane; }
+    const fixed &Get_Recharge_IronCurtain() const { return RechargeIronCurtain; }
+    const fixed &Get_Recharge_GPS() const { return RechargeGPS; }
+    const fixed &Get_Recharge_Nuke() const { return RechargeNuke; }
+    const fixed &Get_Speak_Delay() const { return SpeakDelay; }
+    const fixed &Get_Damage_Delay() const { return DamageDelay; }
+    int Get_Gravity() const { return Gravity; }
+    int Gap_Radius() const { return GapRadius; }
+    const fixed &Gap_Regen_Interval() const { return GapRegenInterval; }
+    int16_t Radar_Jam_Radius() const { return RadarJamRadius; }
+    MPHType Get_Incoming() const { return Incoming; }
+    int Min_Damage() const { return MinDamage; }
+    int Max_Damage() const { return MaxDamage; }
     int Get_Repair_Step() const { return RepairStep; }
     const fixed &Get_Repair_Percent() const { return RepairPercent; }
     int Get_Unit_Repair_Step() const { return URepairStep; }
     const fixed &Get_Unit_Repair_Percent() const { return URepairPercent; }
-    const fixed &Get_Timer_Warning() const { return TimerWarning; }
+    const fixed &Get_Repair_Rate() const { return RepairRate; }
+    const fixed &Condition_Green() const { return ConditionGreen; }
+    const fixed &Condition_Yellow() const { return ConditionYellow; }
+    const fixed &Condition_Red() const { return ConditionRed; }
+    const fixed &Idle_Action_Frequency() const { return IdleActionFrequency; }
+    int Bail_Count() const { return BailCount; }
+    int Get_Gold_Value() const { return GoldValue; }
+    int Get_Gem_Value() const { return GemValue; }
+    int Max_Aircraft() const { return MaxAircraft; }
+    int Max_Anim() const { return MaxAnim; }
+    int Max_Building() const { return MaxBuilding; }
+    int Max_Bullet() const { return MaxBullet; }
+    int Max_Factory() const { return MaxFactory; }
+    int Max_Infantry() const { return MaxInfantry; }
+    int Max_Overlay() const { return MaxOverlay; }
+    int Max_Smudge() const { return MaxSmudge; }
+    int Max_Team() const { return MaxTeam; }
+    int Max_TeamType() const { return MaxTeamType; }
+    int Max_Template() const { return MaxTemplate; }
+    int Max_Terrain() const { return MaxTerrain; }
+    int Max_Trigger() const { return MaxTrigger; }
+    int Max_Unit() const { return MaxUnit; }
+    int Max_Vessel() const { return MaxVessel; }
+    int Max_Projectile() const { return MaxProjectile; }
+    int Max_Weapon() const { return MaxWeapon; }
+    int Max_Warhead() const { return MaxWarhead; }
+    int Max_TrigType() const { return MaxTrigType; }
+    int16_t Close_Enough_Distance() const { return CloseEnough; }
+    int16_t Stray_Distance() const { return Stray; }
+    int16_t Crush_Distance() const { return Crush; }
+    int16_t Crate_Radius() const { return CrateRadius; }
+    int16_t Homing_Scatter_Distance() const { return HomingScatter; }
+    int16_t Ballistic_Scatter_Distance() const { return BallisticScatter; }
+    const fixed &Refund_Percent() const { return RefundPercent; }
+    const fixed &Get_Iron_Curtain() const { return IronCurtain; }
+    int Bridge_Strength() const { return BridgeStrength; }
+    const fixed &Get_Build_Speed() const { return BuildSpeed; }
+    const fixed &C4_Delay() const { return C4Delay; }
+    int Credit_Reserve() const { return CreditReserve; }
+    const fixed &Path_Delay() const { return PathDelay; }
+    const fixed &Movie_Time() const { return MovieTime; }
+    int Chrono_Tech_Level() const { return ChronoTechLevel; }
+    int16_t Ore_Near_Scan() const { return OreNearScan; }
+    int16_t Ore_Far_Scan() const { return OreFarScan; }
+    const fixed &Engineer_Damage() const { return EngineerDamage; }
+    const fixed &Engineer_Capture_Level() const { return EngineerCaptureLevel; }
+    const fixed &Chrono_Tank_Duration() const { return ChronoTankDuration; }
+    int MTank_Distance() const { return MTankDistance; }
+    int Carrier_Launch_Delay() const { return CarrierLaunchDelay; }
+    const fixed &Quake_Unit_Damage() const { return QuakeUnitDamage; }
+    const fixed &Quake_Vessel_Damage() const { return QuakeVesselDamage; }
+    const fixed &Quake_Building_Damage() const { return QuakeBuildingDamage; }
+    int Quake_Infantry_Damage() const { return QuakeInfantryDamage; }
+    int Quake_Delay() const { return QuakeDelay; }
+    const fixed &Cloak_Delay() const { return CloakDelay; }
 
 private:
     fixed TurboBoost;
@@ -116,12 +295,12 @@ private:
     fixed SubmergeDelay;
     fixed GameSpeedBias;
     fixed BaseBias;
-    int16_t VortexRange;
+    lepton_t VortexRange;
     MPHType VortexSpeed;
     int VortexDamage;
     fixed VortexChance;
     fixed ExpSpread;
-    int16_t FireSupress;
+    lepton_t FireSupress;
     int ParaTech;
     int SpyPlaneTech;
     int ParabombTech;
@@ -131,7 +310,7 @@ private:
     CrateType WaterCrate;
     int CrateMinimum;
     int CrateMaximum;
-    int16_t LZScanRadius; // Maximum radius to scan for alternate landing zone if otherwise occupied.
+    lepton_t LZScanRadius; // Maximum radius to scan for alternate landing zone if otherwise occupied.
     int MPlayerMoney;
     int MPlayerMaxMoney;
 #ifndef CHRONOSHIFT_NO_BITFIELDS
@@ -144,7 +323,7 @@ private:
             bool MPlayerBases : 1; // 2
             bool MPlayerOreGrows : 1; // 4
             bool MPlayerCrates : 1; // 8
-            bool MPlayerBit16 : 1; // 16
+            bool MPlayerAIPlayers : 1; // 16
             bool MPlayerCaptureTheFlag : 1; // 32
         };
         int MPBitfield;
@@ -154,10 +333,10 @@ private:
     bool MPlayerBases; // 2
     bool MPlayerOreGrows; // 4
     bool MPlayerCrates; // 8
-    bool MPlayerBit16; // 16
+    bool MPlayerAIPlayers; // 16
     bool MPlayerCaptureTheFlag; // 32
 #endif
-    int16_t DropZoneRadius;
+    lepton_t DropZoneRadius;
     fixed MessageDelay;
     fixed SavourDelay;
     int AVMineDamage;
@@ -247,7 +426,7 @@ private:
     int Gravity;
     int GapRadius;
     fixed GapRegenInterval;
-    int16_t RadarJamRadius;
+    lepton_t RadarJamRadius;
     MPHType Incoming;
     int MinDamage;
     int MaxDamage;
@@ -282,12 +461,12 @@ private:
     int MaxWeapon;
     int MaxWarhead;
     int MaxTrigType;
-    int16_t CloseEnough;
-    int16_t Stray;
-    int16_t Crush;
-    int16_t CrateRadius;
-    int16_t HomingScatter;
-    int16_t BallisticScatter;
+    lepton_t CloseEnough;
+    lepton_t Stray;
+    lepton_t Crush;
+    lepton_t CrateRadius;
+    lepton_t HomingScatter;
+    lepton_t BallisticScatter;
     fixed RefundPercent;
     fixed IronCurtain;
     int BridgeStrength;
@@ -297,14 +476,14 @@ private:
     fixed PathDelay;
     fixed MovieTime;
     int ChronoTechLevel;
-    int16_t OreNearScan;
-    int16_t OreFarScan;
+    lepton_t OreNearScan;
+    lepton_t OreFarScan;
 #ifndef CHRONOSHIFT_STANDALONE
     static fixed &EngineerDamage;
     static fixed &EngineerCaptureLevel;
     static fixed &ChronoTankDuration;
     static int &MTankDistance;
-    static int CarrierLaunchDelay;
+    static int &CarrierLaunchDelay;
     static fixed &QuakeUnitDamage;
     static fixed QuakeVesselDamage;
     static fixed &QuakeBuildingDamage;
@@ -319,13 +498,13 @@ private:
     fixed EngineerCaptureLevel;
     fixed ChronoTankDuration;
     int MTankDistance;
-    int CarrierLaunchDelay; // New additional control for RA++.
+    int CarrierLaunchDelay;
     fixed QuakeUnitDamage;
-    fixed QuakeVesselDamage; // New additional control for RA++.
+    fixed QuakeVesselDamage; // New additional control for Chronoshift.
     fixed QuakeBuildingDamage;
     int QuakeInfantryDamage;
     int QuakeDelay;
-    fixed CloakDelay; // New additional control for RA++.
+    fixed CloakDelay; // New additional control for Chronoshift, to be moved next to SubmergeDelay.
 #endif
 };
 
