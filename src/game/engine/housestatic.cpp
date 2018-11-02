5d16660670279590da8c51c022f8636951efb81d
CCHyper <12141592+CCHyper@users.noreply.github.com>
2018-11-02 22:29:07 +0000

    Initial HouseStaticClass.

---
+++ src/game/engine/housestatic.cpp
@@ -0,0 +1,120 @@
+/**
+ * @file
+ *
+ * @author CCHyper
+ *
+ * @brief Class holding information on static house info.
+ *
+ * @copyright Chronoshift is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "housestatic.h"
+#include "gameini.h"
+#include "rules.h"
+#include "scenario.h"
+
+HouseStaticClass::HouseStaticClass() :
+    IQ(0),
+    TechLevel(1),
+    Allies(0),
+    MaxUnit(Rule.Max_Unit() / Rule.Max_Players()),
+    MaxBuilding(Rule.Max_Building() / Rule.Max_Players()),
+    MaxInfantry(Rule.Max_Infantry() / Rule.Max_Players()),
+    MaxVessel(Rule.Max_Vessel() / Rule.Max_Players()),
+    MaxAircraft(Rule.Max_Unit() / Rule.Max_Players()),
+    Credits(0),
+    Edge(SOURCE_NORTH)
+{
+}
+
+HouseStaticClass::HouseStaticClass(const HouseStaticClass &that) :
+    IQ(that.IQ),
+    TechLevel(that.TechLevel),
+    Allies(that.Allies),
+    MaxUnit(that.MaxUnit),
+    MaxBuilding(that.MaxBuilding),
+    MaxInfantry(that.MaxInfantry),
+    MaxVessel(that.MaxVessel),
+    MaxAircraft(that.MaxAircraft),
+    Credits(that.Credits),
+    Edge(that.Edge)
+{
+}
+
+bool HouseStaticClass::Read_INI(GameINIClass &ini, char const *section)
+{
+    if (ini.Section_Present(section)) {
+        MaxBuilding = ini.Get_Int(section, "MaxBuilding", MaxBuilding);
+        MaxUnit = ini.Get_Int(section, "MaxUnit", MaxUnit);
+        MaxInfantry = ini.Get_Int(section, "MaxInfantry", MaxInfantry);
+        MaxAircraft = ini.Get_Int(section, "MaxAircraft", MaxAircraft);
+        MaxVessel = ini.Get_Int(section, "MaxVessel", MaxVessel);
+
+        if (MaxVessel <= 0) {
+            MaxVessel = MaxUnit;
+        }
+
+        TechLevel = ini.Get_Int(section, "TechLevel", TechLevel);
+
+        Credits = (ini.Get_Int(section, "Credits", Credits) * 100);
+
+        IQ = ini.Get_Int(section, "IQ", IQ);
+        if (IQ > Rule.IQ_Controls().MaxIQLevels) {
+            IQ = 1;
+        }
+
+        Edge = ini.Get_SourceType(section, "Edge", SOURCE_NORTH);
+
+        return true;
+    }
+
+    return false;
+}
+
+void HouseStaticClass::Write_INI(GameINIClass &ini, char const *section)
+{
+    if (ini.Section_Present(section)) {
+        // Default instance for comparison purposes.
+        HouseStaticClass statichouse;
+
+        if (Credits != statichouse.Credits) {
+            ini.Put_Int(section, "Credits", (Credits / 100));
+        }
+
+        if (Edge != statichouse.Edge) {
+            ini.Put_SourceType(section, "Edge", Edge);
+        }
+
+        if (MaxBuilding > 0 && MaxBuilding != statichouse.MaxBuilding) {
+            ini.Put_Int(section, "MaxBuilding", MaxBuilding);
+        }
+
+        if (MaxUnit > 0 && MaxUnit != statichouse.MaxUnit) {
+            ini.Put_Int(section, "MaxUnit", MaxUnit);
+        }
+
+        if (MaxInfantry > 0 && MaxInfantry != statichouse.MaxInfantry) {
+            ini.Put_Int(section, "MaxInfantry", MaxInfantry);
+        }
+
+        if (MaxAircraft > 0 && MaxAircraft != statichouse.MaxAircraft) {
+            ini.Put_Int(section, "MaxAircraft", MaxAircraft);
+        }
+
+        if (MaxVessel > 0 && MaxVessel != statichouse.MaxVessel) {
+            ini.Put_Int(section, "MaxAircraft", MaxVessel);
+        }
+
+        if (IQ != statichouse.IQ) {
+            ini.Put_Int(section, "IQ", IQ);
+        }
+
+        if (Edge != statichouse.Edge) {
+            ini.Put_Int(section, "Edge", Edge);
+        }
+    }
+}
---
+++ src/game/engine/housestatic.h
@@ -0,0 +1,89 @@
+/**
+ * @file
+ *
+ * @author CCHyper
+ *
+ * @brief Class holding information on static house info.
+ *
+ * @copyright Chronoshift is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#pragma once
+
+#ifndef HOUSESTATIC_H
+#define HOUSESTATIC_H
+
+#include "always.h"
+#include "gametypes.h"
+#include "heap.h"
+#include "source.h"
+
+class NoInitClass;
+class GameINIClass;
+
+class HouseStaticClass
+{
+public:
+    HouseStaticClass();
+    HouseStaticClass(const HouseStaticClass &that);
+    HouseStaticClass(const NoInitClass &noinit) {}
+    ~HouseStaticClass() {}
+
+    bool Read_INI(GameINIClass &ini, const char *section);
+    void Write_INI(GameINIClass &ini, const char *section);
+
+    int Get_IQ() const { return IQ; }
+    int Get_TechLevel() const { return TechLevel; }
+    int32_t Get_Allies() const { return Allies; }
+    int Get_MaxUnit() const { return MaxUnit; }
+    int Get_MaxBuilding() const { return MaxBuilding; }
+    int Get_MaxInfantry() const { return MaxInfantry; }
+    int Get_MaxVessel() const { return MaxVessel; }
+    int Get_MaxAircraft() const { return MaxAircraft; }
+    int Get_Credits() const { return Credits; }
+    SourceType Get_Edge() const { return Edge; }
+
+private:
+    // The initial IQ for this house.
+    int IQ;
+
+    // The tech level of this house, which in turn defines which techno
+    // objects the side may produce. Any object with a tech level higher than
+    // this value will not be available to the house.
+    int TechLevel;
+
+    // A a bit list of sides this house is allied with.
+    // (This only means the side can not attack these sides, not the other way
+    // around unless they both define each other as allies!)
+    // NOTE: This only means that this house is allied with the houses set here.
+    int32_t Allies;
+
+    // Maximum number of vessels this house can produce.
+    int MaxUnit;
+
+    // Maximum number of buildings this house can produce.
+    int MaxBuilding;
+
+    // Maximum number of infantry this house can produce.
+    int MaxInfantry;
+
+    // Maximum number of vessels this house can produce.
+    int MaxVessel;
+
+    // Maximum number of aircraft this house can produce.
+    int MaxAircraft;
+
+    // Defines the inital amount of credits this house starts with.
+    // NOTE: Multiplied by 100 [a value of 10 would mean 1000 credits].
+    int Credits;
+
+    // The closest edge of the map from which reinforcment triggers, superweapons
+    // and other possible actions start from. [see SourceType enum].
+    SourceType Edge;
+};
+
+#endif // HOUSESTATIC_H
