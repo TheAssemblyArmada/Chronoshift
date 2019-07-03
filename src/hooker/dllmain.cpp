fcb1849bf19987a16ab7736b2f663d60cd70e018
CCHyper <12141592+CCHyper@users.noreply.github.com>
2019-07-03 19:25:21 +0100

    Implements game main loop functions.

--- src/hooker/dllmain.cpp
+++ src/hooker/dllmain.cpp
@@ -68,6 +68,7 @@
 #include "version.h"
 #include "wsa.h"
 #include "xordelta.h"
+#include "gameloop.h"
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -156,6 +157,7 @@ void Setup_Hooks()
     // Hook_Function(0x005E5200, (void *)0x005E53CD); // This one forces better interpolation algo.
     Init::Hook_Me();
     Hook_Function(0x004AAC58, &Force_CD_Available);
+    Hook_Function(0x004A7C74, Main_Loop);
 }
 
 StaticInitObject::StaticInitObject()
