68757c817b14b1a676d9939b8fb34a758db47416
OmniBlade <nadia_xy@hotmail.com>
2019-07-10 12:35:46 +0100

    Rewrite of Shape_Dimensions.

--- src/game/gfx/shape.h
+++ src/game/gfx/shape.h
@@ -140,7 +140,7 @@ inline void Hook_Me()
 #ifdef COMPILER_WATCOM
     Hook_Function(0x005B48EC, Build_Frame);
     Hook_Function(0x005AB354, Buffer_Frame_To_Page);
-    //Hook_Function(0x004A9AB8, Shape_Dimensions);  // Currently broken.
+    Hook_Function(0x004A9AB8, Shape_Dimensions);
 #endif
 }
 
