810b3fcdc49d6218e54ec052043bd3e9f28dbc1d
CCHyper <12141592+CCHyper@users.noreply.github.com>
2019-10-06 03:44:02 +0100

    Implements UnitClass virtual table and stub functions.

--- src/game/engine/unit.cpp
+++ src/game/engine/unit.cpp
@@ -14,26 +14,599 @@
  *            LICENSE
  */
 #include "unit.h"
+#include "iomap.h"
+#include "team.h"
 
 #ifndef GAME_DLL
 TFixedIHeapClass<UnitClass> g_Units;
 #endif
 
-UnitClass::UnitClass(RTTIType type, int id, HousesType house) :
-    DriveClass(type, id, house)
+/**
+ * @brief 
+ * @address 0x00
+ */
+// TODO: Correct values for members.
+UnitClass::UnitClass(UnitType type, HousesType house) :
+    DriveClass(RTTI_UNIT, g_Units.ID(this), house),
+    m_Type(),
+    m_FlagOwner(),
+    m_IsDumping(),
+    m_Gold(),
+    m_Gems(),
+    m_ToScatter(),
+    m_BailCount(),
+    m_GapGenCellTracker(),
+    m_GapGenCenterCell(),
+    m_V2RearmDelayTimer(),
+    m_TurretFacing()
 {
 }
 
-UnitClass::UnitClass(const UnitClass &that) :
-    DriveClass(that)
-{
-}
-
-UnitClass::UnitClass(const NoInitClass &noinit) :
-    DriveClass(noinit)
-{
-}
-
+/**
+ * @brief 
+ * @address 0x00
+ */
 UnitClass::~UnitClass()
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void *UnitClass::operator new(size_t size)
+{
+    UnitClass *this_ptr = g_Units.Alloc();
+    if (this_ptr != nullptr) {
+        this_ptr->m_IsActive = true;
+    }
+    return this_ptr;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::operator delete(void *ptr)
+{
+    UnitClass *this_ptr = static_cast<UnitClass *>(ptr);
+    if (this_ptr != nullptr) {
+        this_ptr->m_IsActive = false;
+    }
+    g_Units.Free(this_ptr);
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+MoveType UnitClass::Can_Enter_Cell(cell_t cellnum, FacingType facing) const
+{
+    return MoveType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::AI()
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+ActionType UnitClass::What_Action(ObjectClass *object) const
+{
+    return ActionType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+ActionType UnitClass::What_Action(cell_t cellnum) const
+{
+    return ActionType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+coord_t UnitClass::Sort_Y() const
+{
+    return coord_t();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL UnitClass::Limbo()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL UnitClass::Unlimbo(coord_t coord, DirType dir)
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+const int16_t *UnitClass::Overlap_List(BOOL a1) const
+{
+    return nullptr;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Draw_It(int x, int y, WindowNumberType window) const
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Active_Click_With(ActionType action, ObjectClass *object)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Active_Click_With(ActionType action, cell_t cellnum)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+DamageResultType UnitClass::Take_Damage(int &damage, int a2, WarheadType warhead, TechnoClass *object, BOOL a5)
+{
+    return DamageResultType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Scatter(coord_t coord, int a2, BOOL a3)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Per_Cell_Process(PCPType pcp)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+RadioMessageType UnitClass::Receive_Message(RadioClass *radio, RadioMessageType message, target_t &target)
+{
+    return RadioMessageType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int UnitClass::Mission_Guard()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int UnitClass::Mission_Guard_Area()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int UnitClass::Mission_Harvest()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int UnitClass::Mission_Hunt()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int UnitClass::Mission_Move()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int UnitClass::Mission_Unload()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int UnitClass::Mission_Repair()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+DirType UnitClass::Turret_Facing() const
+{
+    return DirType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+DirType UnitClass::Desired_Load_Dir(ObjectClass *object, cell_t &cellnum) const
+{
+    return DirType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+DirType UnitClass::Fire_Direction() const
+{
+    return DirType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+InfantryType UnitClass::Crew_Type() const
+{
+    return InfantryType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+fixed_t UnitClass::Ore_Load() const
+{
+    return fixed_t();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int UnitClass::Pip_Count() const
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+FireErrorType UnitClass::Can_Fire(target_t target, WeaponSlotType weapon) const
+{
+    return FireErrorType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+target_t UnitClass::Greatest_Threat(ThreatType threat)
+{
+    return target_t();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BulletClass *UnitClass::Fire_At(target_t target, WeaponSlotType weapon)
+{
+    return nullptr;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Assign_Destination(target_t dest)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Enter_Idle_Mode(BOOL a1)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL UnitClass::Start_Driver(coord_t &dest)
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL UnitClass::Offload_Ore_Bail()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Approach_Target()
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Overrun_Cell(cell_t cell, int a2)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL UnitClass::Ok_To_Move(DirType dir)
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL UnitClass::Flag_Attach(HousesType house)
+{
+    if (house == HOUSES_NONE || m_FlagOwner != HOUSES_NONE) {
+        return false;
+    }
+
+    m_FlagOwner = house;
+    Mark(MARK_REDRAW);
+
+    return true;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL UnitClass::Flag_Remove()
+{
+    if (m_FlagOwner == HOUSES_NONE) {
+        return false;
+    }
+
+    m_FlagOwner = HOUSES_NONE;
+    Mark(MARK_REDRAW);
+
+    return true;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Init()
+{
+    g_Units.Free_All();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Read_INI(GameINIClass &ini)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Write_INI(GameINIClass &ini)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Rotation_AI()
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL UnitClass::Edge_Of_World_AI()
+{
+    if (Mission != MISSION_GUARD || Map.In_Radar(Coord_To_Cell(m_Coord)) || !m_LockedOnMap) {
+        return false;
+    }
+
+    if (m_Team.Is_Valid()) {
+        m_Team->Set_Bit2_4(true);
+    }
+
+    Stun();
+    delete this;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Reload_AI()
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Firing_AI()
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL UnitClass::Goto_Clear_Spot()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL UnitClass::Try_To_Deploy()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int UnitClass::Shape_Number() const
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL UnitClass::Ore_Check(cell_t &cell, int a2, int a3)
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL UnitClass::Goto_Ore(int a1)
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL UnitClass::Harvesting()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Exit_Repair()
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Transport_Close_Door()
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Transport_Open_Door()
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int UnitClass::Credit_Load() const
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL UnitClass::Should_Crush_It(TechnoClass* object) const
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void UnitClass::Shroud_Regen()
 {
 }
--- src/game/engine/unit.h
+++ src/game/engine/unit.h
@@ -25,31 +25,115 @@
 class UnitClass : public DriveClass
 {
 public:
-    UnitClass(RTTIType type, int id, HousesType house);
-    UnitClass(const UnitClass &that);
-    UnitClass(const NoInitClass &noinit);
+    UnitClass(UnitType type, HousesType house);
+    UnitClass(const NoInitClass& noinit) : DriveClass(noinit), m_V2RearmDelayTimer(noinit), m_TurretFacing(noinit) {}
     virtual ~UnitClass();
 
+    void *operator new(size_t size);
+    void *operator new(size_t size, void *ptr) { return ptr; }
+    void operator delete(void *ptr);
+#ifndef COMPILER_WATCOM // Watcom doesn't like this, MSVC/GCC does.
+    void operator delete(void *ptr, void *place) {}
+#endif
+
+    // AbstractClass
+    virtual MoveType Can_Enter_Cell(cell_t cellnum, FacingType facing = FACING_NONE) const override;
+    virtual void AI() override;
+
     // ObjectClass
+    virtual ActionType What_Action(ObjectClass *object) const override;
+    virtual ActionType What_Action(cell_t cellnum) const override;
     virtual const UnitTypeClass &Class_Of() const override { return *m_Type; }
+    virtual coord_t Sort_Y() const override;
+    virtual BOOL Limbo() override;
+    virtual BOOL Unlimbo(coord_t coord, DirType dir = DIR_NORTH) override;
+    virtual const int16_t *Overlap_List(BOOL a1 = false) const override;
+    virtual void Draw_It(int x, int y, WindowNumberType window) const override;
+    virtual void Active_Click_With(ActionType action, ObjectClass *object) override;
+    virtual void Active_Click_With(ActionType action, cell_t cellnum) override;
+    virtual DamageResultType Take_Damage(int &damage, int a2, WarheadType warhead, TechnoClass *object = nullptr, BOOL a5 = false) override;
+    virtual void Scatter(coord_t coord = 0, int a2 = 0, BOOL a3 = false) override;
+    virtual void Per_Cell_Process(PCPType pcp) override;
+    virtual RadioMessageType Receive_Message(RadioClass *radio, RadioMessageType message, target_t &target) override;
+
+    // MissionClass
+    virtual int Mission_Guard() override;
+    virtual int Mission_Guard_Area() override;
+    virtual int Mission_Harvest() override;
+    virtual int Mission_Hunt() override;
+    virtual int Mission_Move() override;
+    virtual int Mission_Unload() override;
+    virtual int Mission_Repair() override;
+
+    // TechnoClass
+    virtual DirType Turret_Facing() const override;
+    virtual DirType Desired_Load_Dir(ObjectClass *object, cell_t &cellnum) const override;
+    virtual DirType Fire_Direction() const override;
+    virtual InfantryType Crew_Type() const override;
+    virtual fixed_t Ore_Load() const override;
+    virtual int Pip_Count() const override;
+    virtual FireErrorType Can_Fire(target_t target, WeaponSlotType weapon = WEAPON_SLOT_PRIMARY) const override;
+    virtual target_t Greatest_Threat(ThreatType threat) override;
+    virtual BulletClass *Fire_At(target_t target, WeaponSlotType weapon = WEAPON_SLOT_PRIMARY) override;
+    virtual void Assign_Destination(target_t dest) override;
+    virtual void Enter_Idle_Mode(BOOL a1 = false) override;
+
+    // FootClass
+    virtual BOOL Start_Driver(coord_t &dest) override;
+    virtual BOOL Offload_Ore_Bail() override;
+    virtual void Approach_Target() override;
+
+    // DriveClass
+    virtual void Overrun_Cell(cell_t cell, int a2) override;
+    virtual BOOL Ok_To_Move(DirType dir) override;
+
+    // UnitClass
+    BOOL Flag_Attach(HousesType house);
+    BOOL Flag_Remove();
 
     UnitType What_Type() const { return m_Type->What_Type(); }
+
+    static void Init();
+    static void Read_INI(GameINIClass &ini);
+    static void Write_INI(GameINIClass &ini);
+
+private:
+    void Rotation_AI();
+    BOOL Edge_Of_World_AI();
+    void Reload_AI();
+    void Firing_AI();
+    BOOL Goto_Clear_Spot();
+    BOOL Try_To_Deploy();
+    int Shape_Number() const;
+    BOOL Ore_Check(cell_t &cell, int a2, int a3);
+    BOOL Goto_Ore(int a1);
+    BOOL Harvesting();
+    void Exit_Repair();
+    void Transport_Close_Door();
+    void Transport_Open_Door();
+    int Credit_Load() const;
+    BOOL Should_Crush_It(TechnoClass *object) const;
+    void Shroud_Regen();
 
 private:
     GamePtr<UnitTypeClass> m_Type;
     HousesType m_FlagOwner;
 #ifndef CHRONOSHIFT_NO_BITFIELDS
-    BOOL m_Bit1 : 1; // 1
-    BOOL m_Bit2 : 1; // 2
-    BOOL m_Bit4 : 1; // 4
-    BOOL m_Bit8 : 1; // 8
-    BOOL m_Bit16 : 1; // 16
+    // TODO: We think the MAD Tank might abuse the Gold/Gems values.
+    //unsigned char IsDumping : 1;
+    //unsigned char Gold : 5;
+    //unsigned char Gems : 5;
+    //unsigned char ToScatter : 1;
+    BOOL m_IsDumping : 1;
+    int m_Gold : 5;
+    int m_Gems : 5;
+    BOOL m_ToScatter : 1;
 #else
-    bool m_Bit1;
-    bool m_Bit2;
-    bool m_Bit4;
-    bool m_Bit8;
-    bool m_Bit16;
+    // TODO: Names subject to change, just guesses at this time.
+    bool m_IsDumping;
+    int m_Gold;
+    int m_Gems;
+    bool m_ToScatter;
 #endif
     unsigned int m_BailCount;
     unsigned int m_GapGenCellTracker;
