c1d61c8658edb41c94a8d8ab394cb680cec94c3f
CCHyper <12141592+CCHyper@users.noreply.github.com>
2019-10-06 03:44:26 +0100

    Implements AircraftClass virtual table and stub functions.

--- src/game/engine/aircraft.cpp
+++ src/game/engine/aircraft.cpp
@@ -14,26 +14,593 @@
  *            LICENSE
  */
 #include "aircraft.h"
+#include "infantry.h"
+#include "scenario.h"
 
 #ifndef GAME_DLL
 TFixedIHeapClass<AircraftClass> g_Aircraft;
 #endif
 
-AircraftClass::AircraftClass(RTTIType type, int id, HousesType house) :
-    FootClass(type, id, house)
+/**
+ * @brief 
+ * @address 0x00
+ */
+// TODO: Correct values for members.
+AircraftClass::AircraftClass(AircraftType type, HousesType house) :
+    FootClass(RTTI_AIRCRAFT, g_Aircraft.ID(this), house),
+    m_Flyer(),
+    m_Type(),
+    m_field_14A(),
+    m_field_14C(),
+    m_Bit1(),
+    m_Bit2(),
+    m_Bit4(),
+    m_Bit8(),
+    m_Bit16(),
+    m_field_154(),
+    m_field_155(),
+    m_field_15E()
 {
 }
 
-AircraftClass::AircraftClass(const AircraftClass &that) :
-    FootClass(that)
-{
-}
-
-AircraftClass::AircraftClass(const NoInitClass &noinit) :
-    FootClass(noinit)
-{
-}
-
+/**
+ * @brief 
+ * @address 0x00
+ */
 AircraftClass::~AircraftClass()
 {
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void *AircraftClass::operator new(size_t size)
+{
+    return nullptr;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::operator delete(void *ptr)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+MoveType AircraftClass::Can_Enter_Cell(cell_t cellnum, FacingType facing) const
+{
+    return MoveType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::AI()
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+ActionType AircraftClass::What_Action(ObjectClass *object) const
+{
+    return ActionType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+ActionType AircraftClass::What_Action(cell_t cellnum) const
+{
+    return ActionType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+LayerType AircraftClass::In_Which_Layer() const
+{
+    return LayerType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+coord_t AircraftClass::Sort_Y() const
+{
+    return coord_t();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL AircraftClass::Unlimbo(coord_t coord, DirType dir)
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int AircraftClass::Exit_Object(TechnoClass *object)
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+const int16_t *AircraftClass::Overlap_List(BOOL a1) const
+{
+    return nullptr;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Draw_It(int x, int y, WindowNumberType window) const
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Look(BOOL a1)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Active_Click_With(ActionType action, ObjectClass *object)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Active_Click_With(ActionType action, cell_t cellnum)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+DamageResultType AircraftClass::Take_Damage(int &damage, int a2, WarheadType warhead, TechnoClass *object, BOOL a5)
+{
+    return DamageResultType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Scatter(coord_t coord, int a2, BOOL a3)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Per_Cell_Process(PCPType pcp)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+RadioMessageType AircraftClass::Receive_Message(RadioClass *radio, RadioMessageType message, target_t &target)
+{
+    return RadioMessageType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int AircraftClass::Mission_Attack()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int AircraftClass::Mission_Guard()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int AircraftClass::Mission_Guard_Area()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int AircraftClass::Mission_Hunt()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int AircraftClass::Mission_Move()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int AircraftClass::Mission_Retreat()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int AircraftClass::Mission_Unload()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int AircraftClass::Mission_Enter()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+DirType AircraftClass::Turret_Facing() const
+{
+    return DirType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+DirType AircraftClass::Desired_Load_Dir(ObjectClass *object, cell_t &cellnum) const
+{
+    return DirType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+DirType AircraftClass::Fire_Direction() const
+{
+    return DirType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int AircraftClass::Pip_Count() const
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Response_Select()
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Response_Move()
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Response_Attack()
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Player_Assign_Mission(MissionType mission, target_t target, target_t dest)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+FireErrorType AircraftClass::Can_Fire(target_t target, WeaponSlotType weapon) const
+{
+    return FireErrorType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BulletClass *AircraftClass::Fire_At(target_t target, WeaponSlotType weapon)
+{
+    return nullptr;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Assign_Destination(target_t dest)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Enter_Idle_Mode(BOOL a1)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Set_Speed(int speed)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+DirType AircraftClass::Pose_Dir() const
+{
+    return DirType();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Init()
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Read_INI(GameINIClass &ini)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Write_INI(GameINIClass &ini)
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int AircraftClass::Shape_Number() const
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Draw_Rotors(int x, int y, WindowNumberType window) const
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int AircraftClass::Paradrop_Cargo()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+target_t AircraftClass::New_LZ(target_t target) const
+{
+    return target_t();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL AircraftClass::Is_LZ_Clear(target_t target) const
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+int AircraftClass::Process_Fly_To(int a1, target_t a2)
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL AircraftClass::Process_Take_Off()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL AircraftClass::Process_Landing()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+target_t AircraftClass::Good_Fire_Location(target_t target) const
+{
+    return target_t();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL AircraftClass::Cell_Seems_Ok(cell_t cell, int a2) const
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+target_t AircraftClass::Good_LZ() const
+{
+    return target_t();
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL AircraftClass::Landing_Takeoff_AI()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL AircraftClass::Edge_Of_World_AI()
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Movement_AI()
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+void AircraftClass::Rotation_AI()
+{
+}
+
+/**
+ * @brief 
+ * @address 0x00
+ */
+BOOL AircraftClass::Counts_As_Civ_Evac(ObjectClass *object)
+{
+    if (object == nullptr) {
+        return false;
+    }
+
+    if (object->What_Am_I() != RTTI_INFANTRY) {
+        return false;
+    }
+
+    InfantryClass *iobjptr = reinterpret_cast<InfantryClass *>(object);
+
+    switch (iobjptr->What_Type()) {
+        case INFANTRY_EINSTEIN:
+        case INFANTRY_GENERAL:
+        case INFANTRY_DELPHI:
+        case INFANTRY_CHAN:
+            return true;
+        default:
+            break;
+    };
+
+    if (Scen.Civ_Evac()) {
+        if (iobjptr->What_Type() == INFANTRY_TANYA) {
+            return true;
+        }
+    }
+
+    if (!iobjptr->Class_Of().Is_Civilian()) {
+        return false;
+    }
+
+    if (!iobjptr->Get_Technician()) {
+        return true;
+
+    }
+
+    return false;
 }
--- src/game/engine/aircraft.h
+++ src/game/engine/aircraft.h
@@ -27,21 +27,96 @@
 class AircraftClass : public FootClass
 {
 public:
-    AircraftClass(RTTIType type, int id, HousesType house);
-    AircraftClass(const AircraftClass &that);
-    AircraftClass(const NoInitClass &noinit);
+    AircraftClass(AircraftType type, HousesType house);
+    AircraftClass(const NoInitClass& noinit) : FootClass(noinit) {}
     virtual ~AircraftClass();
 
+    void *operator new(size_t size);
+    void *operator new(size_t size, void *ptr) { return ptr; }
+    void operator delete(void *ptr);
+#ifndef COMPILER_WATCOM // Watcom doesn't like this, MSVC/GCC does.
+    void operator delete(void *ptr, void *place) {}
+#endif
+
+    // AbstractClass
+    virtual MoveType Can_Enter_Cell(cell_t cellnum, FacingType facing = FACING_NONE) const override;
+    virtual void AI() override;
+
     // ObjectClass
+    virtual ActionType What_Action(ObjectClass *object) const override;
+    virtual ActionType What_Action(cell_t cellnum) const override;
+    virtual LayerType In_Which_Layer() const override;
     virtual const AircraftTypeClass &Class_Of() const override { return *m_Type; }
+    virtual coord_t Sort_Y() const override;
+    virtual BOOL Unlimbo(coord_t coord, DirType dir = DIR_NORTH) override;
+    virtual int Exit_Object(TechnoClass *object);
+    virtual const int16_t *Overlap_List(BOOL a1 = false) const override;
+    virtual void Draw_It(int x, int y, WindowNumberType window) const override;
+    virtual void Look(BOOL a1 = false) override;
+    virtual void Active_Click_With(ActionType action, ObjectClass *object) override;
+    virtual void Active_Click_With(ActionType action, cell_t cellnum) override;
+    virtual DamageResultType Take_Damage(int &damage, int a2, WarheadType warhead, TechnoClass *object = nullptr, BOOL a5 = false) override;
+    virtual void Scatter(coord_t coord = 0, int a2 = 0, BOOL a3 = false) override;
+    virtual void Per_Cell_Process(PCPType pcp);
+    virtual RadioMessageType Receive_Message(RadioClass *radio, RadioMessageType message, target_t &target) override;
+
+    // MissionClass
+    virtual int Mission_Attack() override;
+    virtual int Mission_Guard() override;
+    virtual int Mission_Guard_Area() override;
+    virtual int Mission_Hunt() override;
+    virtual int Mission_Move() override;
+    virtual int Mission_Retreat() override;
+    virtual int Mission_Unload() override;
+    virtual int Mission_Enter() override;
+
+    // TechnoClass
+    virtual DirType Turret_Facing() const override;
+    virtual DirType Desired_Load_Dir(ObjectClass *object, cell_t &cellnum) const override;
+    virtual DirType Fire_Direction() const override;
+    virtual int Pip_Count() const override;
+    virtual void Response_Select() override;
+    virtual void Response_Move() override;
+    virtual void Response_Attack() override;
+    virtual void Player_Assign_Mission(MissionType mission, target_t target = 0, target_t dest = 0) override;
+    virtual FireErrorType Can_Fire(target_t target, WeaponSlotType weapon = WEAPON_SLOT_PRIMARY) const override;
+    virtual BulletClass *Fire_At(target_t target, WeaponSlotType weapon = WEAPON_SLOT_PRIMARY) override;
+    virtual void Assign_Destination(target_t dest) override;
+    virtual void Enter_Idle_Mode(BOOL a1 = false) override;
+    virtual void Set_Speed(int speed) override;
+
+    DirType Pose_Dir() const;
 
     AircraftType What_Type() const { return m_Type->What_Type(); }
 
+    static void Init();
+    static void Read_INI(GameINIClass &ini);
+    static void Write_INI(GameINIClass &ini);
+
 private:
-    FlyClass FlyControl;
+    int Shape_Number() const;
+    void Draw_Rotors(int x, int y, WindowNumberType window) const;
+    int Paradrop_Cargo();
+    target_t New_LZ(target_t target) const;
+    BOOL Is_LZ_Clear(target_t target) const;
+    int Process_Fly_To(int a1, target_t a2);
+    BOOL Process_Take_Off();
+    BOOL Process_Landing();
+    target_t Good_Fire_Location(target_t target) const;
+    BOOL Cell_Seems_Ok(cell_t cell, int a2) const;
+    target_t Good_LZ() const;
+    BOOL Landing_Takeoff_AI();
+    BOOL Edge_Of_World_AI();
+    void Movement_AI();
+    void Rotation_AI();
+
+    static BOOL Counts_As_Civ_Evac(ObjectClass *object);
+
+private:
+    FlyClass m_Flyer;
     GamePtr<AircraftTypeClass> m_Type;
-    FacingClass field_14A;
-    void *field_14C;
+    FacingClass m_field_14A;
+    void *m_field_14C;
 #ifndef CHRONOSHIFT_NO_BITFIELDS
     BOOL m_Bit1 : 1; // 1
     BOOL m_Bit2 : 1; // 2
@@ -55,9 +130,9 @@ private:
     bool m_Bit8;
     bool m_Bit16;
 #endif
-    uint8_t field_154;
-    BasicTimerClass<FrameTimerClass> field_155;
-    uint8_t field_15E;
+    uint8_t m_field_154;
+    BasicTimerClass<FrameTimerClass> m_field_155;
+    uint8_t m_field_15E;
 };
 
 #ifdef GAME_DLL
