5c7599e7f075d6232108f272ad93d19956786802
Toms <tomsons26lv@gmail.com>
2019-12-30 22:10:13 +0200

    Sets UnitClass virtuals as final.

--- src/game/engine/unit.h
+++ src/game/engine/unit.h
@@ -31,55 +31,55 @@ public:
     virtual ~UnitClass();
 
     // AbstractClass
-    virtual MoveType Can_Enter_Cell(cell_t cellnum, FacingType facing = FACING_NONE) const override;
+    virtual MoveType Can_Enter_Cell(cell_t cellnum, FacingType facing = FACING_NONE) const final;
 
     // ObjectClass
-    virtual ActionType What_Action(ObjectClass *object) const override;
-    virtual ActionType What_Action(cell_t cellnum) const override;
-    virtual const UnitTypeClass &Class_Of() const override { return *m_Class; }
-    virtual coord_t Sort_Y() const override;
-    virtual BOOL Limbo() override;
-    virtual BOOL Unlimbo(coord_t coord, DirType dir = DIR_NORTH) override;
-    virtual const int16_t *Overlap_List(BOOL a1 = false) const override;
-    virtual void Draw_It(int x_pos, int y_pos, WindowNumberType window) const override;
-    virtual void Active_Click_With(ActionType action, ObjectClass *object) override;
-    virtual void Active_Click_With(ActionType action, cell_t cellnum) override;
+    virtual ActionType What_Action(ObjectClass *object) const final;
+    virtual ActionType What_Action(cell_t cellnum) const final;
+    virtual const UnitTypeClass &Class_Of() const final { return *m_Class; }
+    virtual coord_t Sort_Y() const final;
+    virtual BOOL Limbo() final;
+    virtual BOOL Unlimbo(coord_t coord, DirType dir = DIR_NORTH) final;
+    virtual const int16_t *Overlap_List(BOOL a1 = false) const final;
+    virtual void Draw_It(int x_pos, int y_pos, WindowNumberType window) const final;
+    virtual void Active_Click_With(ActionType action, ObjectClass *object) final;
+    virtual void Active_Click_With(ActionType action, cell_t cellnum) final;
     virtual DamageResultType Take_Damage(
-        int &damage, int a2, WarheadType warhead, TechnoClass *object = nullptr, BOOL a5 = false) override;
-    virtual void Scatter(coord_t coord = 0, BOOL a2 = false, BOOL a3 = false) override;
-    virtual void Per_Cell_Process(PCPType pcp) override;
-    virtual RadioMessageType Receive_Message(RadioClass *radio, RadioMessageType message, target_t &target) override;
+        int &damage, int a2, WarheadType warhead, TechnoClass *object = nullptr, BOOL a5 = false) final;
+    virtual void Scatter(coord_t coord = 0, BOOL a2 = false, BOOL a3 = false) final;
+    virtual void Per_Cell_Process(PCPType pcp) final;
+    virtual RadioMessageType Receive_Message(RadioClass *radio, RadioMessageType message, target_t &target) final;
 
     // MissionClass
-    virtual int Mission_Guard() override;
-    virtual int Mission_Guard_Area() override;
-    virtual int Mission_Harvest() override;
-    virtual int Mission_Hunt() override;
-    virtual int Mission_Move() override;
-    virtual int Mission_Unload() override;
-    virtual int Mission_Repair() override;
+    virtual int Mission_Guard() final;
+    virtual int Mission_Guard_Area() final;
+    virtual int Mission_Harvest() final;
+    virtual int Mission_Hunt() final;
+    virtual int Mission_Move() final;
+    virtual int Mission_Unload() final;
+    virtual int Mission_Repair() final;
 
     // TechnoClass
-    virtual DirType Turret_Facing() const override;
-    virtual DirType Desired_Load_Dir(ObjectClass *object, cell_t &cellnum) const override;
-    virtual DirType Fire_Direction() const override;
-    virtual InfantryType Crew_Type() const override;
-    virtual fixed_t Ore_Load() const override;
-    virtual int Pip_Count() const override;
-    virtual FireErrorType Can_Fire(target_t target, WeaponSlotType weapon = WEAPON_SLOT_PRIMARY) const override;
-    virtual target_t Greatest_Threat(ThreatType threat) override;
-    virtual BulletClass *Fire_At(target_t target, WeaponSlotType weapon = WEAPON_SLOT_PRIMARY) override;
-    virtual void Assign_Destination(target_t dest) override;
-    virtual void Enter_Idle_Mode(BOOL a1 = false) override;
+    virtual DirType Turret_Facing() const final;
+    virtual DirType Desired_Load_Dir(ObjectClass *object, cell_t &cellnum) const final;
+    virtual DirType Fire_Direction() const final;
+    virtual InfantryType Crew_Type() const final;
+    virtual fixed_t Ore_Load() const final;
+    virtual int Pip_Count() const final;
+    virtual FireErrorType Can_Fire(target_t target, WeaponSlotType weapon = WEAPON_SLOT_PRIMARY) const final;
+    virtual target_t Greatest_Threat(ThreatType threat) final;
+    virtual BulletClass *Fire_At(target_t target, WeaponSlotType weapon = WEAPON_SLOT_PRIMARY) final;
+    virtual void Assign_Destination(target_t dest) final;
+    virtual void Enter_Idle_Mode(BOOL a1 = false) final;
 
     // FootClass
-    virtual BOOL Start_Driver(coord_t &dest) override;
-    virtual BOOL Offload_Ore_Bail() override;
-    virtual void Approach_Target() override;
+    virtual BOOL Start_Driver(coord_t &dest) final;
+    virtual BOOL Offload_Ore_Bail() final;
+    virtual void Approach_Target() final;
 
     // DriveClass
-    virtual void Overrun_Cell(cell_t cell, int a2) override;
-    virtual BOOL Ok_To_Move(DirType dir) override;
+    virtual void Overrun_Cell(cell_t cell, int a2) final;
+    virtual BOOL Ok_To_Move(DirType dir) final;
 
     BOOL Edge_Of_World_AI();
     BOOL Flag_Attach(HousesType house);
